82aec65f2e1786fc91cd6f51e1c610b27eb9abde
louis-jackman <77608615+louis-jackman@users.noreply.github.com>
2022-11-01 06:40:56 -0500

    Vanilla JS

--- loadScript.js
+++ loadScript.js
@@ -1,17 +1,16 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
 const loadScript = (window, url) => {
     return new Promise(async (resolve, reject) => {
         try {
             const script = window.document.createElement("script");
             script.defer = true;
+
             script.src = url;
             window.document.head.appendChild(script);
             script.addEventListener("load", () => resolve());
-        }
-        catch (e) {
+        } catch (e) {
             reject();
         }
     });
-};
-exports.default = loadScript;
+}
+
+export default loadScript
