a7ebf86a21e35cdff94a36066051924897954772
louis-jackman <77608615+louis-jackman@users.noreply.github.com>
2022-11-01 14:00:03 -0500

    SignalTile update

--- reusable/SignalTile.js
+++ reusable/SignalTile.js
@@ -19,12 +19,10 @@ const SignalTile = (pill, vehicle) => {
         }
         </style>
         <div style="height: 100%; padding: 10px; display: flex; flex-direction: column;">
-            <div style="display: flex; height: 100%; background-image: linear-gradient(#1d4882, #688cc3); color: white; padding: 15px; border-radius: 15px; user-select: none; align-items: center;" data-signal="${pill.signal}">
-                <div style="display: flex; flex-direction: column; overflow: hidden; width: 100%;">
-                    <div style="margin-bottom: 10px; overflow: hidden;text-overflow: ellipsis; font-size: 0.75em;" title="${pill.signal}">${pill.signal}</div>
-                    <div style="font-size: 1.1em;" class="signal-value">No Value Yet</div>
-                </div>
-                ${pill.icon && `<div style="margin-left: auto;height: 100%;margin-left: 10px;margin-right: 4px;margin-top: 4px;"><i style="font-size: 1.3em;" class="fa-solid fa-${pill.icon}"></i></div>`}
+            <div style="display: flex; flex-direction: column; height: 100%; background-image: linear-gradient(#1d4882, #688cc3); color: white; padding: 15px; border-radius: 15px; user-select: none; align-items: center;" data-signal="${pill.signal}">
+                <div style="margin-bottom: 10px; font-size: 0.75em;" title="${pill.signal}">${pill.signal}</div>
+                <div style="font-size: 1.1em;" class="signal-value">No Value Yet</div>
+                ${pill.icon && `<div style="margin-top: auto;"><i style="font-size: 2.5em;" class="fa-solid fa-${pill.icon}"></i></div>`}
             </div>
         </div>
         `)
