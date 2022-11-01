6f035f8587c3d02f65a8b08ccbb5f512b9286e8f
louis-jackman <77608615+louis-jackman@users.noreply.github.com>
2022-11-01 12:23:28 -0500

    Update ExampleConsumer.js

--- ExampleConsumer.js
+++ ExampleConsumer.js
@@ -38,22 +38,22 @@ const plugin = ({widgets, vehicle, simulator}) => {
         )
     )
 
-    widgets.register(
-        "GoogleMapsSignalConsumer",
-        GoogleMapsFromSignal(
-            [
-                {
-                    "lat": 48.149497,
-                    "lng": 11.523194
-                },
-                {
-                    "lat": 50.445168,
-                    "lng": 11.020569
-                },
-            ],
-            vehicle
-        )
-    )
+    // widgets.register(
+    //     "GoogleMapsSignalConsumer",
+    //     GoogleMapsFromSignal(
+    //         [
+    //             {
+    //                 "lat": 48.149497,
+    //                 "lng": 11.523194
+    //             },
+    //             {
+    //                 "lat": 50.445168,
+    //                 "lng": 11.020569
+    //             },
+    //         ],
+    //         vehicle
+    //     )
+    // )
 
 }
 
