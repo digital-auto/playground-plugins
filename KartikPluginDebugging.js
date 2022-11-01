376092e4fe8cb867da4aa801c540a3246dd5ad15
louis-jackman <77608615+louis-jackman@users.noreply.github.com>
2022-11-01 01:55:31 -0500

    Update KartikPluginDebugging.js

--- KartikPluginDebugging.js
+++ KartikPluginDebugging.js
@@ -14,45 +14,45 @@ const plugin = ({ widgets, simulator, vehicle }) => {
 
 
           
-            var x_co = [0, 5, 10, 15, 20, 25, 30];
+            // var x_co = [0, 5, 10, 15, 20, 25, 30];
         
-            var y_co = [0, 15, 30, 47, 50, 45, 30]
+            // var y_co = [0, 15, 30, 47, 50, 45, 30]
           
 
 
-            const data = {
-                labels: x_co,
-                datasets: [{
-                    label: 'Rear Brake Temperature',
-                    backgroundColor: '#34d2eb',
-                    borderColor: '#34d2eb',
-                    data: y_co
-                }]
-            };
+            // const data = {
+            //     labels: x_co,
+            //     datasets: [{
+            //         label: 'Rear Brake Temperature',
+            //         backgroundColor: '#34d2eb',
+            //         borderColor: '#34d2eb',
+            //         data: y_co
+            //     }]
+            // };
 
-            const config = {
-                type: 'line',
-                data: data,
-                options: {
-                    scales: {
-                        y: {
-                            title: {
-                                display: true,
-                                text: 'Temp (in °C)'
-                            }
-                        },
-                        x: {
-                            title: {
-                                display: true,
-                                text: 'Time (in mins.)'
+            // const config = {
+            //     type: 'line',
+            //     data: data,
+            //     options: {
+            //         scales: {
+            //             y: {
+            //                 title: {
+            //                     display: true,
+            //                     text: 'Temp (in °C)'
+            //                 }
+            //             },
+            //             x: {
+            //                 title: {
+            //                     display: true,
+            //                     text: 'Time (in mins.)'
 
-                            }
-                        }
-                    }
-                }
-            };
+            //                 }
+            //             }
+            //         }
+            //     }
+            // };
             
-            loadScript(box.window, "https://cdn.jsdelivr.net/npm/chart.js")
+            // loadScript(box.window, "https://cdn.jsdelivr.net/npm/chart.js")
 
             // const myChart = new Chart(
                 
