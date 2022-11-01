aee23e016a5c4afb6eef8a5e91acf4117c97be7a
louis-jackman <77608615+louis-jackman@users.noreply.github.com>
2022-11-01 09:56:04 -0500

    Seperated SignalData.js from ExampleConsumer.js

---
+++ SignalData.js
@@ -0,0 +1,228 @@
+import SimulatorPlugins from "./reusable/SimulatorPlugins.js"
+
+const plugin = ({widgets, vehicle, simulator}) => {
+    SimulatorPlugins([
+        {
+            "Vehicle.CurrentLocation.Latitude": 48.149497,
+            "Vehicle.CurrentLocation.Longitude": 11.523194,
+            "Vehicle.Speed": 20
+        },
+        {
+            "Vehicle.CurrentLocation.Latitude": 48.191627,
+            "Vehicle.CurrentLocation.Longitude": 11.473755,
+            "Vehicle.Speed": 30
+        },
+        {
+            "Vehicle.CurrentLocation.Latitude": 48.237381,
+            "Vehicle.CurrentLocation.Longitude": 11.457276,
+            "Vehicle.Speed": 40
+        },
+        {
+            "Vehicle.CurrentLocation.Latitude": 48.284922,
+            "Vehicle.CurrentLocation.Longitude": 11.4572,
+            "Vehicle.Speed": 50
+        },
+        {
+            "Vehicle.CurrentLocation.Latitude": 48.341,
+            "Vehicle.CurrentLocation.Longitude": 11.4380,
+            "Vehicle.Speed": 51
+        },
+        {
+            "Vehicle.CurrentLocation.Latitude": 48.3944,
+            "Vehicle.CurrentLocation.Longitude": 11.4682,
+            "Vehicle.Speed": 52
+        },
+        {
+            "Vehicle.CurrentLocation.Latitude": 48.4473,
+            "Vehicle.CurrentLocation.Longitude": 11.481995,
+            "Vehicle.Speed": 54
+        },
+        {
+            "Vehicle.CurrentLocation.Latitude": 48.489209,
+            "Vehicle.CurrentLocation.Longitude": 11.503968,
+            "Vehicle.Speed": 55
+        },
+        {
+            "Vehicle.CurrentLocation.Latitude": 48.567422,
+            "Vehicle.CurrentLocation.Longitude": 11.542420,
+            "Vehicle.Speed": 57
+        },
+        {
+            "Vehicle.CurrentLocation.Latitude": 48.600127,
+            "Vehicle.CurrentLocation.Longitude": 11.572632,
+            "Vehicle.Speed": 58
+        },
+        {
+            "Vehicle.CurrentLocation.Latitude": 48.643700,
+            "Vehicle.CurrentLocation.Longitude": 11.564393,
+            "Vehicle.Speed": 59
+        },
+        {
+            "Vehicle.CurrentLocation.Latitude": 48.65821,
+            "Vehicle.CurrentLocation.Longitude": 11.487488,
+            "Vehicle.Speed": 60
+        },
+        {
+            "Vehicle.CurrentLocation.Latitude": 48.747037,
+            "Vehicle.CurrentLocation.Longitude": 11.4407,
+            "Vehicle.Speed": 61
+        },
+        {
+            "Vehicle.CurrentLocation.Latitude": 48.8803148,
+            "Vehicle.CurrentLocation.Longitude": 11.358399,
+            "Vehicle.Speed": 62
+        },
+        {
+            "Vehicle.CurrentLocation.Latitude": 48.821235,
+            "Vehicle.CurrentLocation.Longitude": 11.281495,
+            "Vehicle.Speed": 63
+        },
+        {
+            "Vehicle.CurrentLocation.Latitude": 48.844738,
+            "Vehicle.CurrentLocation.Longitude": 11.210084,
+            "Vehicle.Speed": 64
+        },
+        {
+            "Vehicle.CurrentLocation.Latitude": 48.879070,
+            "Vehicle.CurrentLocation.Longitude": 11.152405,
+            "Vehicle.Speed": 65
+        },
+        {
+            "Vehicle.CurrentLocation.Latitude": 48.868231,
+            "Vehicle.CurrentLocation.Longitude": 11.105713,
+            "Vehicle.Speed": 66
+        },
+        {
+            "Vehicle.CurrentLocation.Latitude": 48.888100,
+            "Vehicle.CurrentLocation.Longitude": 11.042542,
+            "Vehicle.Speed": 66
+        },
+        {
+            "Vehicle.CurrentLocation.Latitude": 48.913377,
+            "Vehicle.CurrentLocation.Longitude": 10.987610,
+            "Vehicle.Speed": 67
+        },
+        {
+            "Vehicle.CurrentLocation.Latitude": 49.003549,
+            "Vehicle.CurrentLocation.Longitude": 10.949158,
+            "Vehicle.Speed": 67
+        },
+        {
+            "Vehicle.CurrentLocation.Latitude": 49.068371,
+            "Vehicle.CurrentLocation.Longitude": 10.962891,
+            "Vehicle.Speed": 67
+        },
+        {
+            "Vehicle.CurrentLocation.Latitude": 49.138500,
+            "Vehicle.CurrentLocation.Longitude": 11.012330,
+            "Vehicle.Speed": 68
+        },
+        {
+            "Vehicle.CurrentLocation.Latitude": 49.181607,
+            "Vehicle.CurrentLocation.Longitude": 11.0095,
+            "Vehicle.Speed": 69
+        },
+        {
+            "Vehicle.CurrentLocation.Latitude": 49.221088,
+            "Vehicle.CurrentLocation.Longitude": 11.067261,
+            "Vehicle.Speed": 70
+        },
+        {
+            "Vehicle.CurrentLocation.Latitude": 48.85850429451804,
+            "Vehicle.CurrentLocation.Longitude": 9.125898683591739,
+            "Vehicle.Speed": 70
+        },
+        {
+            "Vehicle.CurrentLocation.Latitude": 49.357237,
+            "Vehicle.CurrentLocation.Longitude": 11.050782,
+            "Vehicle.Speed": 71
+        },
+        {
+            "Vehicle.CurrentLocation.Latitude": 49.414451,
+            "Vehicle.CurrentLocation.Longitude": 11.039795,
+            "Vehicle.Speed": 0
+        },
+        {
+            "Vehicle.CurrentLocation.Latitude": 49.487658,
+            "Vehicle.CurrentLocation.Longitude": 10.971131,
+            "Vehicle.Speed": 0
+        },
+        {
+            "Vehicle.CurrentLocation.Latitude": 49.534026,
+            "Vehicle.CurrentLocation.Longitude": 10.995850,
+            "Vehicle.Speed": 0
+        },
+        {
+            "Vehicle.CurrentLocation.Latitude": 49.642638,
+            "Vehicle.CurrentLocation.Longitude": 11.020569,
+            "Vehicle.Speed": 0
+        },
+        {
+            "Vehicle.CurrentLocation.Latitude": 49.672865,
+            "Vehicle.CurrentLocation.Longitude": 11.048035,
+            "Vehicle.Speed": 0
+        },
+        {
+            "Vehicle.CurrentLocation.Latitude": 49.751010,
+            "Vehicle.CurrentLocation.Longitude": 11.061768,
+            "Vehicle.Speed": 0
+        },
+        {
+            "Vehicle.CurrentLocation.Latitude": 49.782942,
+            "Vehicle.CurrentLocation.Longitude": 11.034302,
+            "Vehicle.Speed": 0
+        },
+        {
+            "Vehicle.CurrentLocation.Latitude": 49.841430,
+            "Vehicle.CurrentLocation.Longitude": 10.976624,
+            "Vehicle.Speed": 0
+        },
+        {
+            "Vehicle.CurrentLocation.Latitude": 49.949357,
+            "Vehicle.CurrentLocation.Longitude": 10.880494,
+            "Vehicle.Speed": 79
+        },
+        {
+            "Vehicle.CurrentLocation.Latitude": 49.998817,
+            "Vehicle.CurrentLocation.Longitude": 10.913453,
+            "Vehicle.Speed": 86
+        },
+        {
+            "Vehicle.CurrentLocation.Latitude": 50.057044,
+            "Vehicle.CurrentLocation.Longitude": 10.946412,
+            "Vehicle.Speed": 81
+        },
+        {
+            "Vehicle.CurrentLocation.Latitude": 50.166248,
+            "Vehicle.CurrentLocation.Longitude": 10.954651,
+            "Vehicle.Speed": 89
+        },
+        {
+            "Vehicle.CurrentLocation.Latitude": 50.217242,
+            "Vehicle.CurrentLocation.Longitude": 11.006837,
+            "Vehicle.Speed": 95
+        },
+        {
+            "Vehicle.CurrentLocation.Latitude": 50.280470,
+            "Vehicle.CurrentLocation.Longitude": 11.017823,
+            "Vehicle.Speed": 94
+        },
+        {
+            "Vehicle.CurrentLocation.Latitude": 50.340107,
+            "Vehicle.CurrentLocation.Longitude": 11.017823,
+            "Vehicle.Speed": 93
+        },
+        {
+            "Vehicle.CurrentLocation.Latitude": 50.380408,
+            "Vehicle.CurrentLocation.Longitude": 11.034302,
+            "Vehicle.Speed": 91
+        },
+        {
+            "Vehicle.CurrentLocation.Latitude": 50.445168,
+            "Vehicle.CurrentLocation.Longitude": 11.020569,
+            "Vehicle.Speed": 90
+        },
+    ], simulator)
+}
+
+export default plugin
