818ded78456164d4247ed7805feaf41175dcf5b2
Bajaj <piyush.bajaj@ferdinand-steinbeis-institut.de>
2022-11-02 15:25:55 +0100

    background color parameter

--- reusable/MobileNotifications.js
+++ reusable/MobileNotifications.js
@@ -6,10 +6,22 @@
 ** all the options are mandatory but if you do not want to show the notification based on
 ** VSS signal values, then you can send apis, vehicle and refresh as null
 */
-const MobileNotifications = async ({box, apis = null, vehicle = null, refresh = null}) => {
+const MobileNotifications = async ({box, apis = null, vehicle = null, refresh = null, backgroundColor = unset}) => {
     const container = document.createElement("div")
     container.setAttribute("style", `height: 100%; width: 100%;`)
     container.innerHTML = (`
+    <style>
+		@import url('https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,400;0,700;1,400;1,700&display=swap');
+        * {
+            box-sizing: border-box;
+        }
+        body {
+            font-family: 'Lato', sans-serif;
+            color:#ffffe3;
+            background-color:${backgroundColor};
+            text-align:center;            
+        }
+		</style>
     <div style="max-width: fit-content; margin: 0 auto; position: relative;">
     <img src="https://firebasestorage.googleapis.com/v0/b/digital-auto.appspot.com/o/media%2FDashboardPhone.png?alt=media&token=d361018a-b4b3-42c0-8ef0-16c9e70fd9c7" style="height: 100%; width: 100%; object-fit: contain;">
         <div class="smartphone-text" style="position: absolute; color: white; font-family: 'Lato'; width: 100%; top: 0; height: 100%; box-sizing: border-box; padding-top: 25px; padding-right: 12px; padding-left: 12px; padding-bottom: 25px;"></div>
