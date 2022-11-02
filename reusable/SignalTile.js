174c15efd4e476eb09717c9a62505d3dbe577918
louis-jackman <77608615+louis-jackman@users.noreply.github.com>
2022-11-02 03:18:24 -0500

    Update SignalTile.js

--- reusable/SignalTile.js
+++ reusable/SignalTile.js
@@ -23,7 +23,7 @@ const SignalTile = (pill, vehicle) => {
         </style>
         <div style="height: 100%; padding: 10px; display: flex; flex-direction: column;">
             <div style="display: flex; flex-direction: column; height: 100%; background-image: linear-gradient(#1d4882, #688cc3); color: white; padding: 35px 20px; border-radius: 25px 15px; user-select: none; align-items: center;" data-signal="${pill.signal}">
-                <div style="width: fit-content; margin-bottom: 10px;font-size: 1.1em;font-weight: bold;margin-top: auto;overflow: hidden;text-overflow: ellipsis;width: 100%;" title="${label}">${label}</div>
+                <div style="margin-bottom: 10px;font-size: 1.1em;font-weight: bold;margin-top: auto;overflow: hidden;text-overflow: ellipsis;width: fit-content;" title="${label}">${label}</div>
                 <div style="font-size: 1.1em; margin-bottom: 25px;" class="signal-value">No Value Yet</div>
                 ${pill.icon ? `<div style="margin-top: auto;"><i style="font-size: 3em;" class="fa-solid fa-${pill.icon}"></i></div>` : ``}
             </div>
