9b5deef49fd4e3854001cb855ee5375e7aac9eda
louis-jackman <77608615+louis-jackman@users.noreply.github.com>
2022-11-02 03:46:58 -0500

    Update SignalData.js

--- SignalData.js
+++ SignalData.js
@@ -5,223 +5,399 @@ const plugin = ({widgets, vehicle, simulator}) => {
         {
             "Vehicle.CurrentLocation.Latitude": 48.149497,
             "Vehicle.CurrentLocation.Longitude": 11.523194,
-            "Vehicle.Speed": 20
+            "Vehicle.Speed": 20,
+            "Vehicle.ETA": 61200,
+            "Vehicle.Trailer.Chassis.Axle.Row1.Temperature": 7,
+            "Vehicle.Trailer.Chassis.Axle.Row2.Temperature": 7,
+            "Vehicle.Trailer.CargoSpace.Door.Left.isOpen": false
         },
         {
             "Vehicle.CurrentLocation.Latitude": 48.191627,
             "Vehicle.CurrentLocation.Longitude": 11.473755,
-            "Vehicle.Speed": 30
+            "Vehicle.Speed": 30,
+            "Vehicle.ETA": 57600,
+            "Vehicle.Trailer.Chassis.Axle.Row1.Temperature": 4,
+            "Vehicle.Trailer.Chassis.Axle.Row2.Temperature": 4,
+            "Vehicle.Trailer.CargoSpace.Door.Left.isOpen": false
         },
         {
             "Vehicle.CurrentLocation.Latitude": 48.237381,
             "Vehicle.CurrentLocation.Longitude": 11.457276,
-            "Vehicle.Speed": 40
+            "Vehicle.Speed": 40,
+            "Vehicle.ETA": 59400,
+            "Vehicle.Trailer.Chassis.Axle.Row1.Temperature": 6,
+            "Vehicle.Trailer.Chassis.Axle.Row2.Temperature": 6,
+            "Vehicle.Trailer.CargoSpace.Door.Left.isOpen": false
         },
         {
             "Vehicle.CurrentLocation.Latitude": 48.284922,
             "Vehicle.CurrentLocation.Longitude": 11.4572,
-            "Vehicle.Speed": 50
+            "Vehicle.Speed": 50,
+            "Vehicle.ETA": 59292,
+            "Vehicle.Trailer.Chassis.Axle.Row1.Temperature": 6,
+            "Vehicle.Trailer.Chassis.Axle.Row2.Temperature": 6,
+            "Vehicle.Trailer.CargoSpace.Door.Left.isOpen": false
         },
         {
             "Vehicle.CurrentLocation.Latitude": 48.341,
             "Vehicle.CurrentLocation.Longitude": 11.4380,
-            "Vehicle.Speed": 51
+            "Vehicle.Speed": 51,
+            "Vehicle.ETA": 58176,
+            "Vehicle.Trailer.Chassis.Axle.Row1.Temperature": 5,
+            "Vehicle.Trailer.Chassis.Axle.Row2.Temperature": 5,
+            "Vehicle.Trailer.CargoSpace.Door.Left.isOpen": false
         },
         {
             "Vehicle.CurrentLocation.Latitude": 48.3944,
             "Vehicle.CurrentLocation.Longitude": 11.4682,
-            "Vehicle.Speed": 52
+            "Vehicle.Speed": 52,
+            "Vehicle.ETA": 56124,
+            "Vehicle.Trailer.Chassis.Axle.Row1.Temperature": 6,
+            "Vehicle.Trailer.Chassis.Axle.Row2.Temperature": 6,
+            "Vehicle.Trailer.CargoSpace.Door.Left.isOpen": false
         },
         {
             "Vehicle.CurrentLocation.Latitude": 48.4473,
             "Vehicle.CurrentLocation.Longitude": 11.481995,
-            "Vehicle.Speed": 54
+            "Vehicle.Speed": 54,
+            "Vehicle.ETA": 55980,
+            "Vehicle.Trailer.Chassis.Axle.Row1.Temperature": 7,
+            "Vehicle.Trailer.Chassis.Axle.Row2.Temperature": 7,
+            "Vehicle.Trailer.CargoSpace.Door.Left.isOpen": false
         },
         {
             "Vehicle.CurrentLocation.Latitude": 48.489209,
             "Vehicle.CurrentLocation.Longitude": 11.503968,
-            "Vehicle.Speed": 55
+            "Vehicle.Speed": 55,
+            "Vehicle.ETA": 55620,
+            "Vehicle.Trailer.Chassis.Axle.Row1.Temperature": 7,
+            "Vehicle.Trailer.Chassis.Axle.Row2.Temperature": 7,
+            "Vehicle.Trailer.CargoSpace.Door.Left.isOpen": false
         },
         {
             "Vehicle.CurrentLocation.Latitude": 48.567422,
             "Vehicle.CurrentLocation.Longitude": 11.542420,
-            "Vehicle.Speed": 57
+            "Vehicle.Speed": 57,
+            "Vehicle.ETA": 55260,
+            "Vehicle.Trailer.Chassis.Axle.Row1.Temperature": 7,
+            "Vehicle.Trailer.Chassis.Axle.Row2.Temperature": 7,
+            "Vehicle.Trailer.CargoSpace.Door.Left.isOpen": false
         },
         {
             "Vehicle.CurrentLocation.Latitude": 48.600127,
             "Vehicle.CurrentLocation.Longitude": 11.572632,
-            "Vehicle.Speed": 58
+            "Vehicle.Speed": 58,
+            "Vehicle.ETA": 55080,
+            "Vehicle.Trailer.Chassis.Axle.Row1.Temperature": 8,
+            "Vehicle.Trailer.Chassis.Axle.Row2.Temperature": 8,
+            "Vehicle.Trailer.CargoSpace.Door.Left.isOpen": false
         },
         {
             "Vehicle.CurrentLocation.Latitude": 48.643700,
             "Vehicle.CurrentLocation.Longitude": 11.564393,
-            "Vehicle.Speed": 59
+            "Vehicle.Speed": 59,
+            "Vehicle.ETA": 54720,
+            "Vehicle.Trailer.Chassis.Axle.Row1.Temperature": 8,
+            "Vehicle.Trailer.Chassis.Axle.Row2.Temperature": 8,
+            "Vehicle.Trailer.CargoSpace.Door.Left.isOpen": false
         },
         {
             "Vehicle.CurrentLocation.Latitude": 48.65821,
             "Vehicle.CurrentLocation.Longitude": 11.487488,
-            "Vehicle.Speed": 60
+            "Vehicle.Speed": 60,
+            "Vehicle.ETA": 52164,
+            "Vehicle.Trailer.Chassis.Axle.Row1.Temperature": 9,
+            "Vehicle.Trailer.Chassis.Axle.Row2.Temperature": 9,
+            "Vehicle.Trailer.CargoSpace.Door.Left.isOpen": false
         },
         {
             "Vehicle.CurrentLocation.Latitude": 48.747037,
             "Vehicle.CurrentLocation.Longitude": 11.4407,
-            "Vehicle.Speed": 61
+            "Vehicle.Speed": 61,
+            "Vehicle.ETA": 52380,
+            "Vehicle.Trailer.Chassis.Axle.Row1.Temperature": 10,
+            "Vehicle.Trailer.Chassis.Axle.Row2.Temperature": 10,
+            "Vehicle.Trailer.CargoSpace.Door.Left.isOpen": false
         },
         {
             "Vehicle.CurrentLocation.Latitude": 48.8803148,
             "Vehicle.CurrentLocation.Longitude": 11.358399,
-            "Vehicle.Speed": 62
+            "Vehicle.Speed": 62,
+            "Vehicle.ETA": 52200,
+            "Vehicle.Trailer.Chassis.Axle.Row1.Temperature": 15,
+            "Vehicle.Trailer.Chassis.Axle.Row2.Temperature": 15,
+            "Vehicle.Trailer.CargoSpace.Door.Left.isOpen": false
         },
         {
             "Vehicle.CurrentLocation.Latitude": 48.821235,
             "Vehicle.CurrentLocation.Longitude": 11.281495,
-            "Vehicle.Speed": 63
+            "Vehicle.Speed": 63,
+            "Vehicle.ETA": 52092,
+            "Vehicle.Trailer.Chassis.Axle.Row1.Temperature": 12,
+            "Vehicle.Trailer.Chassis.Axle.Row2.Temperature": 12,
+            "Vehicle.Trailer.CargoSpace.Door.Left.isOpen": false
         },
         {
             "Vehicle.CurrentLocation.Latitude": 48.844738,
             "Vehicle.CurrentLocation.Longitude": 11.210084,
-            "Vehicle.Speed": 64
+            "Vehicle.Speed": 64,
+            "Vehicle.ETA": 51444,
+            "Vehicle.Trailer.Chassis.Axle.Row1.Temperature": 10,
+            "Vehicle.Trailer.Chassis.Axle.Row2.Temperature": 10,
+            "Vehicle.Trailer.CargoSpace.Door.Left.isOpen": false
         },
         {
             "Vehicle.CurrentLocation.Latitude": 48.879070,
             "Vehicle.CurrentLocation.Longitude": 11.152405,
-            "Vehicle.Speed": 65
+            "Vehicle.Speed": 65,
+            "Vehicle.ETA": 50976,
+            "Vehicle.Trailer.Chassis.Axle.Row1.Temperature": 8,
+            "Vehicle.Trailer.Chassis.Axle.Row2.Temperature": 8,
+            "Vehicle.Trailer.CargoSpace.Door.Left.isOpen": false
         },
         {
             "Vehicle.CurrentLocation.Latitude": 48.868231,
             "Vehicle.CurrentLocation.Longitude": 11.105713,
-            "Vehicle.Speed": 66
+            "Vehicle.Speed": 66,
+            "Vehicle.ETA": 50796,
+            "Vehicle.Trailer.Chassis.Axle.Row1.Temperature": 5,
+            "Vehicle.Trailer.Chassis.Axle.Row2.Temperature": 5,
+            "Vehicle.Trailer.CargoSpace.Door.Left.isOpen": false
         },
         {
             "Vehicle.CurrentLocation.Latitude": 48.888100,
             "Vehicle.CurrentLocation.Longitude": 11.042542,
-            "Vehicle.Speed": 66
+            "Vehicle.Speed": 66,
+            "Vehicle.ETA": 50472,
+            "Vehicle.Trailer.Chassis.Axle.Row1.Temperature": 14,
+            "Vehicle.Trailer.Chassis.Axle.Row2.Temperature": 14,
+            "Vehicle.Trailer.CargoSpace.Door.Left.isOpen": false
         },
         {
             "Vehicle.CurrentLocation.Latitude": 48.913377,
             "Vehicle.CurrentLocation.Longitude": 10.987610,
-            "Vehicle.Speed": 67
+            "Vehicle.Speed": 67,
+            "Vehicle.ETA": 50400,
+            "Vehicle.Trailer.Chassis.Axle.Row1.Temperature": 15,
+            "Vehicle.Trailer.Chassis.Axle.Row2.Temperature": 15,
+            "Vehicle.Trailer.CargoSpace.Door.Left.isOpen": false
         },
         {
             "Vehicle.CurrentLocation.Latitude": 49.003549,
             "Vehicle.CurrentLocation.Longitude": 10.949158,
-            "Vehicle.Speed": 67
+            "Vehicle.Speed": 67,
+            "Vehicle.ETA": 48924,
+            "Vehicle.Trailer.Chassis.Axle.Row1.Temperature": 19,
+            "Vehicle.Trailer.Chassis.Axle.Row2.Temperature": 19,
+            "Vehicle.Trailer.CargoSpace.Door.Left.isOpen": false
         },
         {
             "Vehicle.CurrentLocation.Latitude": 49.068371,
             "Vehicle.CurrentLocation.Longitude": 10.962891,
-            "Vehicle.Speed": 67
+            "Vehicle.Speed": 67,
+            "Vehicle.ETA": 48780,
+            "Vehicle.Trailer.Chassis.Axle.Row1.Temperature": 19,
+            "Vehicle.Trailer.Chassis.Axle.Row2.Temperature": 19,
+            "Vehicle.Trailer.CargoSpace.Door.Left.isOpen": false
         },
         {
             "Vehicle.CurrentLocation.Latitude": 49.138500,
             "Vehicle.CurrentLocation.Longitude": 11.012330,
-            "Vehicle.Speed": 68
+            "Vehicle.Speed": 68,
+            "Vehicle.ETA": 45180,
+            "Vehicle.Trailer.Chassis.Axle.Row1.Temperature": 20,
+            "Vehicle.Trailer.Chassis.Axle.Row2.Temperature": 20,
+            "Vehicle.Trailer.CargoSpace.Door.Left.isOpen": false
         },
         {
             "Vehicle.CurrentLocation.Latitude": 49.181607,
             "Vehicle.CurrentLocation.Longitude": 11.0095,
-            "Vehicle.Speed": 69
+            "Vehicle.Speed": 69,
+            "Vehicle.ETA": 48195,
+            "Vehicle.Trailer.Chassis.Axle.Row1.Temperature": 22,
+            "Vehicle.Trailer.Chassis.Axle.Row2.Temperature": 22,
+            "Vehicle.Trailer.CargoSpace.Door.Left.isOpen": false
         },
         {
             "Vehicle.CurrentLocation.Latitude": 49.221088,
             "Vehicle.CurrentLocation.Longitude": 11.067261,
-            "Vehicle.Speed": 70
+            "Vehicle.Speed": 70,
+            "Vehicle.ETA": 52020,
+            "Vehicle.Trailer.Chassis.Axle.Row1.Temperature": 23,
+            "Vehicle.Trailer.Chassis.Axle.Row2.Temperature": 23,
+            "Vehicle.Trailer.CargoSpace.Door.Left.isOpen": false
         },
         {
             "Vehicle.CurrentLocation.Latitude": 48.85850429451804,
             "Vehicle.CurrentLocation.Longitude": 9.125898683591739,
-            "Vehicle.Speed": 70
+            "Vehicle.Speed": 70,
+            "Vehicle.ETA": 44820,
+            "Vehicle.Trailer.Chassis.Axle.Row1.Temperature": 25,
+            "Vehicle.Trailer.Chassis.Axle.Row2.Temperature": 25,
+            "Vehicle.Trailer.CargoSpace.Door.Left.isOpen": false
         },
         {
             "Vehicle.CurrentLocation.Latitude": 49.357237,
             "Vehicle.CurrentLocation.Longitude": 11.050782,
-            "Vehicle.Speed": 71
+            "Vehicle.Speed": 71,
+            "Vehicle.ETA": 44460,
+            "Vehicle.Trailer.Chassis.Axle.Row1.Temperature": 26,
+            "Vehicle.Trailer.Chassis.Axle.Row2.Temperature": 26,
+            "Vehicle.Trailer.CargoSpace.Door.Left.isOpen": false
         },
         {
             "Vehicle.CurrentLocation.Latitude": 49.414451,
             "Vehicle.CurrentLocation.Longitude": 11.039795,
-            "Vehicle.Speed": 0
+            "Vehicle.Speed": 0,
+            "Vehicle.ETA": 0,
+            "Vehicle.Trailer.Chassis.Axle.Row1.Temperature": 22,
+            "Vehicle.Trailer.Chassis.Axle.Row2.Temperature": 22,
+            "Vehicle.Trailer.CargoSpace.Door.Left.isOpen": false
         },
         {
             "Vehicle.CurrentLocation.Latitude": 49.487658,
             "Vehicle.CurrentLocation.Longitude": 10.971131,
-            "Vehicle.Speed": 0
+            "Vehicle.Speed": 0,
+            "Vehicle.ETA": 0,
+            "Vehicle.Trailer.Chassis.Axle.Row1.Temperature": 21,
+            "Vehicle.Trailer.Chassis.Axle.Row2.Temperature": 21,
+            "Vehicle.Trailer.CargoSpace.Door.Left.isOpen": true
         },
         {
             "Vehicle.CurrentLocation.Latitude": 49.534026,
             "Vehicle.CurrentLocation.Longitude": 10.995850,
-            "Vehicle.Speed": 0
+            "Vehicle.Speed": 0,
+            "Vehicle.ETA": 0,
+            "Vehicle.Trailer.Chassis.Axle.Row1.Temperature": 20,
+            "Vehicle.Trailer.Chassis.Axle.Row2.Temperature": 20,
+            "Vehicle.Trailer.CargoSpace.Door.Left.isOpen": true
         },
         {
             "Vehicle.CurrentLocation.Latitude": 49.642638,
             "Vehicle.CurrentLocation.Longitude": 11.020569,
-            "Vehicle.Speed": 0
+            "Vehicle.Speed": 0,
+            "Vehicle.ETA": 0,
+            "Vehicle.Trailer.Chassis.Axle.Row1.Temperature": 17,
+            "Vehicle.Trailer.Chassis.Axle.Row2.Temperature": 17,
+            "Vehicle.Trailer.CargoSpace.Door.Left.isOpen": true
         },
         {
             "Vehicle.CurrentLocation.Latitude": 49.672865,
             "Vehicle.CurrentLocation.Longitude": 11.048035,
-            "Vehicle.Speed": 0
+            "Vehicle.Speed": 0,
+            "Vehicle.ETA": 0,
+            "Vehicle.Trailer.Chassis.Axle.Row1.Temperature": 15,
+            "Vehicle.Trailer.Chassis.Axle.Row2.Temperature": 15,
+            "Vehicle.Trailer.CargoSpace.Door.Left.isOpen": false
         },
         {
             "Vehicle.CurrentLocation.Latitude": 49.751010,
             "Vehicle.CurrentLocation.Longitude": 11.061768,
-            "Vehicle.Speed": 0
+            "Vehicle.Speed": 0,
+            "Vehicle.ETA": 0,
+            "Vehicle.Trailer.Chassis.Axle.Row1.Temperature": 12,
+            "Vehicle.Trailer.Chassis.Axle.Row2.Temperature": 12,
+            "Vehicle.Trailer.CargoSpace.Door.Left.isOpen": false
         },
         {
             "Vehicle.CurrentLocation.Latitude": 49.782942,
             "Vehicle.CurrentLocation.Longitude": 11.034302,
-            "Vehicle.Speed": 0
+            "Vehicle.Speed": 0,
+            "Vehicle.ETA": 0,
+            "Vehicle.Trailer.Chassis.Axle.Row1.Temperature": 10,
+            "Vehicle.Trailer.Chassis.Axle.Row2.Temperature": 10,
+            "Vehicle.Trailer.CargoSpace.Door.Left.isOpen": false
         },
         {
             "Vehicle.CurrentLocation.Latitude": 49.841430,
             "Vehicle.CurrentLocation.Longitude": 10.976624,
-            "Vehicle.Speed": 0
+            "Vehicle.Speed": 0,
+            "Vehicle.ETA": 0,
+            "Vehicle.Trailer.Chassis.Axle.Row1.Temperature": 8,
+            "Vehicle.Trailer.Chassis.Axle.Row2.Temperature": 8,
+            "Vehicle.Trailer.CargoSpace.Door.Left.isOpen": false
         },
         {
             "Vehicle.CurrentLocation.Latitude": 49.949357,
             "Vehicle.CurrentLocation.Longitude": 10.880494,
-            "Vehicle.Speed": 79
+            "Vehicle.Speed": 79,
+            "Vehicle.ETA": 39600,
+            "Vehicle.Trailer.Chassis.Axle.Row1.Temperature": 12,
+            "Vehicle.Trailer.Chassis.Axle.Row2.Temperature": 12,
+            "Vehicle.Trailer.CargoSpace.Door.Left.isOpen": false
         },
         {
             "Vehicle.CurrentLocation.Latitude": 49.998817,
             "Vehicle.CurrentLocation.Longitude": 10.913453,
-            "Vehicle.Speed": 86
+            "Vehicle.Speed": 86,
+            "Vehicle.ETA": 32400,
+            "Vehicle.Trailer.Chassis.Axle.Row1.Temperature": 13,
+            "Vehicle.Trailer.Chassis.Axle.Row2.Temperature": 13,
+            "Vehicle.Trailer.CargoSpace.Door.Left.isOpen": true
         },
         {
             "Vehicle.CurrentLocation.Latitude": 50.057044,
             "Vehicle.CurrentLocation.Longitude": 10.946412,
-            "Vehicle.Speed": 81
+            "Vehicle.Speed": 81,
+            "Vehicle.ETA": 30100,
+            "Vehicle.Trailer.Chassis.Axle.Row1.Temperature": 16,
+            "Vehicle.Trailer.Chassis.Axle.Row2.Temperature": 16,
+            "Vehicle.Trailer.CargoSpace.Door.Left.isOpen": false
         },
         {
             "Vehicle.CurrentLocation.Latitude": 50.166248,
             "Vehicle.CurrentLocation.Longitude": 10.954651,
-            "Vehicle.Speed": 89
+            "Vehicle.Speed": 89,
+            "Vehicle.ETA": 27036,
+            "Vehicle.Trailer.Chassis.Axle.Row1.Temperature": 17,
+            "Vehicle.Trailer.Chassis.Axle.Row2.Temperature": 17,
+            "Vehicle.Trailer.CargoSpace.Door.Left.isOpen": false
         },
         {
             "Vehicle.CurrentLocation.Latitude": 50.217242,
             "Vehicle.CurrentLocation.Longitude": 11.006837,
-            "Vehicle.Speed": 95
+            "Vehicle.Speed": 95,
+            "Vehicle.ETA": 25920,
+            "Vehicle.Trailer.Chassis.Axle.Row1.Temperature": 18,
+            "Vehicle.Trailer.Chassis.Axle.Row2.Temperature": 18,
+            "Vehicle.Trailer.CargoSpace.Door.Left.isOpen": false
         },
         {
             "Vehicle.CurrentLocation.Latitude": 50.280470,
             "Vehicle.CurrentLocation.Longitude": 11.017823,
-            "Vehicle.Speed": 94
+            "Vehicle.Speed": 94,
+            "Vehicle.ETA": 26244,
+            "Vehicle.Trailer.Chassis.Axle.Row1.Temperature": 19,
+            "Vehicle.Trailer.Chassis.Axle.Row2.Temperature": 19,
+            "Vehicle.Trailer.CargoSpace.Door.Left.isOpen": false
         },
         {
             "Vehicle.CurrentLocation.Latitude": 50.340107,
             "Vehicle.CurrentLocation.Longitude": 11.017823,
-            "Vehicle.Speed": 93
+            "Vehicle.Speed": 93,
+            "Vehicle.ETA": 26460,
+            "Vehicle.Trailer.Chassis.Axle.Row1.Temperature": 20,
+            "Vehicle.Trailer.Chassis.Axle.Row2.Temperature": 20,
+            "Vehicle.Trailer.CargoSpace.Door.Left.isOpen": false
         },
         {
             "Vehicle.CurrentLocation.Latitude": 50.380408,
             "Vehicle.CurrentLocation.Longitude": 11.034302,
-            "Vehicle.Speed": 91
+            "Vehicle.Speed": 91,
+            "Vehicle.ETA": 26712,
+            "Vehicle.Trailer.Chassis.Axle.Row1.Temperature": 22,
+            "Vehicle.Trailer.Chassis.Axle.Row2.Temperature": 22,
+            "Vehicle.Trailer.CargoSpace.Door.Left.isOpen": false
         },
         {
             "Vehicle.CurrentLocation.Latitude": 50.445168,
             "Vehicle.CurrentLocation.Longitude": 11.020569,
-            "Vehicle.Speed": 90
-        },
+            "Vehicle.Speed": 90,
+            "Vehicle.ETA": 26820,
+            "Vehicle.Trailer.Chassis.Axle.Row1.Temperature": 24,
+            "Vehicle.Trailer.Chassis.Axle.Row2.Temperature": 24,
+            "Vehicle.Trailer.CargoSpace.Door.Left.isOpen": false
+        }
     ], simulator)
 }
 
