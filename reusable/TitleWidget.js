8b35b0fba022eee752302fcf13e22bb207b7bf53
louis-jackman <77608615+louis-jackman@users.noreply.github.com>
2022-11-03 11:58:43 -0500

    Update TitleWidget.js

--- reusable/TitleWidget.js
+++ reusable/TitleWidget.js
@@ -1,6 +1,7 @@
-const TitleWidget = (title) => {
+const TitleWidget = (title, size = 1) => {
     return (box) => {
         const container = document.createElement("div")
+        container.style.fontSize = size.toString() + "em"
         container.innerHTML = (`
         <style>
 		@import url('https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,400;0,700;1,400;1,700&display=swap');
@@ -13,7 +14,7 @@ const TitleWidget = (title) => {
 		</style>
         `)
         const div = document.createElement("div")
-        div.style = "display: flex; width: 100%; height: 100%; align-items: center; justify-content: center; color: #9ca3af; text-align: center; padding: 5px;"
+        div.style = "display: flex; width: 100%; height: 100%; align-items: center; justify-content: center; color: #9ca3af; text-align: center; padding: 5px; user-select: none; font-size: 1.2em;"
         div.textContent = title
         container.appendChild(div)
         box.injectNode(container)
