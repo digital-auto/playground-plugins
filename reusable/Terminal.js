ab0a43344852d62cbdea76c5b17473e73e39bd23
louis-jackman <77608615+louis-jackman@users.noreply.github.com>
2022-11-06 08:57:19 -0600

    Update Terminal.js

--- reusable/Terminal.js
+++ reusable/Terminal.js
@@ -1,24 +1,27 @@
-import loadScript from "./loadScript.js"
-
 const Terminal = ({widgets}) => {
+    let addScript = null
     widgets.register("Terminal", (box) => {
-        box.injectHTML(`
+        const div = document.createElement("div")
+        div.innerHTML = `
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tailwindcss/dist/tailwind.min.css">
-        <div class="flex flex-col text-gray-100 text-sm subpixel-antialiased bg-gray-800 leading-normal overflow-auto h-48 scroll-gray">
+        <div class="flex flex-col text-gray-100 text-sm subpixel-antialiased bg-gray-800 leading-normal overflow-auto h-48 scroll-gray h-full">
             <div class="top flex items-center sticky top-0 left-0 bg-gray-800 px-5 pt-4 pb-2">
                 <div class="select-none">Terminal</div>
             </div>
-            <div class="flex flex-col h-full px-5 text-xs">
-                <div class="mt-2 flex font-mono last:pb-4">
-                    <span class="text-green-400 select-none">&gt;&gt;&gt;</span>
-                    <p class="flex-1 items-center pl-2 whitespace-pre-line">
-                        <span class="">Starting Now: Passenger Welcome - 08:11:51</span>
-                        <span class="">Ending Now: Passenger Welcome</span>
-                    </p>
-                </div>
-            </div>
+            <div class="flex flex-col h-full px-5 text-xs terminal-lines"></div>
         </div>
-        ` )
+        `
+        box.injectNode(div)
+
+        print = (text) => {
+            const line = document.createElement("div")
+            line.className = "flex mt-2 font-mono last:pb-4"
+            line.innerHTML = `
+            <span class="text-green-400 select-none">&gt;&gt;&gt;</span>
+            <p class="flex-1 items-center pl-2 whitespace-pre-line">${text}</p>
+            `
+            div.querySelector(".terminal-lines").appendChild()
+        }
     })
 }
 
