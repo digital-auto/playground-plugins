d228c519a4a5ee3611b6b668f12767ec79a1e259
louis-jackman <77608615+louis-jackman@users.noreply.github.com>
2022-11-08 21:56:21 -0600

    Update DriverScore.js

--- DriverScore.js
+++ DriverScore.js
@@ -1,13 +1,24 @@
 import MobileNotifications from "./reusable/MobileNotifications.js"
+import SignalTile from "./reusable/SignalTile.js"
 
 const DriveScore = ({widgets, vehicle}) => {
+    widgets.register("DriveTimeExceededTile", SignalTile("Driver.DriveTimeExceeded", vehicle))
+
     widgets.register("MobileNotifications", (box) => {
         const {printNotifications} = MobileNotifications({box})
         const intervalId = setInterval(async () => {
-            console.log(await vehicle.Driver.DriveTimeExceeded.get())
-            if (await vehicle.Driver.DriveTimeExceeded.get()) {
-                printNotifications("Drive Time Exceeded!")
+            const [timeExceeded, currentFuelEconomy] = [
+                await vehicle.Driver.DriveTimeExceeded.get(),
+                await vehicle.Powertrain.FuelSystem.CurrentFuelEconomy.get()
+            ]
+            let message = ""
+            if (timeExceeded) {
+                message += "Drive Time Exceeded!\n"
             }
+            if (currentFuelEconomy < 50) {
+                message += "WARNING: CurrentFuelEconomy below 50%!"
+            }
+            printNotifications(message)
         }, 300)
         
         return ( ) => {
