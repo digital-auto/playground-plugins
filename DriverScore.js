8c477ba812cb21d39510a82a6e715e10847ffa2f
louis-jackman <77608615+louis-jackman@users.noreply.github.com>
2022-11-08 23:42:22 -0600

    Update DriverScore.js

--- DriverScore.js
+++ DriverScore.js
@@ -1,10 +1,41 @@
 import MobileNotifications from "./reusable/MobileNotifications.js"
 import SignalTile from "./reusable/SignalTile.js"
+import SignalPills from "./reusable/SignalPills.js"
+import LineChart from "./reusable/LineChart.js"
 
 const DriveScore = ({widgets, vehicle}) => {
     widgets.register("DriveTimeExceededTile", SignalTile({
-        signal: "Driver.DriveTimeExceeded"
+        signal: "Vehicle.Driver.DriveTimeExceeded"
     }, vehicle))
+
+    widgets.register("AccumulatedConsumptionLineChart", LineChart([
+        {
+            signal: "Vehicle.Driver.DriveTimeExceeded"
+        }
+    ], vehicle))
+
+    widgets.register("CurrentFuelEconomyLineChart", LineChart([
+        {
+            signal: "Vehicle.Powertrain.FuelSystem.CurrentFuelEconomy"
+        }
+    ], vehicle))
+
+    widgets.register("CurrentFuelEconomyLineChart", LineChart([
+        {
+            signal: "Vehicle.Powertrain.FuelSystem.CurrentFuelEconomy"
+        }
+    ], vehicle))
+
+    widgets.register("DriverCard", SignalPills([
+        {
+            signal: "Vehicle.Driver.DriverCard.ID",
+            label: "ID"
+        },
+        {
+            signal: "Vehicle.Driver.DriverCard.Name",
+            label: "Name"
+        },
+    ], vehicle))
 
     widgets.register("MobileNotifications", (box) => {
         const {printNotification} = MobileNotifications({box})
