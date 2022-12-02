23d445850a3007ecf362d8c780a15f2bb1d98e05
louis-jackman <77608615+louis-jackman@users.noreply.github.com>
2022-12-02 07:26:48 -0600

    SpreadsheetSimulator

--- reusable/SpreadsheetSimulator.js
+++ reusable/SpreadsheetSimulator.js
@@ -1,78 +1,32 @@
-// Define a promisify function
-function promisify(fn) {
-    return function () {
-        const args = Array.prototype.slice.call(arguments);
-        return new Promise((resolve, reject) => {
-            args.push((err, result) => {
-                if (err) {
-                    reject(err);
-                } else {
-                    resolve(result);
-                }
-            });
-            fn.apply(null, args);
-        });
-    };
-}
+async function getRowsFromSpreadsheet(spreadsheetId, apiKey) {
+    // Fetch the data from the Google Sheets API
+    const response = await fetch(
+        `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values:batchGet?key=${apiKey}`
+    );
 
-// Replace with the ID of your Google Sheets document
-const DOCUMENT_ID = 'your-document-id';
+    // Convert the response to JSON
+    const json = await response.json();
 
-async function getRowsAsObjects() {
-    // Create a new instance of the Google Spreadsheet client
-    const doc = new GoogleSpreadsheet(DOCUMENT_ID);
+    // Get the headers from the first row of the spreadsheet
+    const headers = json.valueRanges[0].values[0];
 
-    // Authenticate with the Google Sheets API
-    try {
-        await promisify(doc.useServiceAccountAuth)();
+    // Get the rows from the remaining values in the value range
+    const rows = json.valueRanges[0].values.slice(1);
 
-        // Get the first sheet in the document
-        const sheets = await promisify(doc.getSheets)(1);
-
-        // Get the names of the columns in the sheet
-        const headers = await promisify(sheets.getCells)({
-            'min-row': 1,
-            'max-row': 1,
-            'min-col': 1,
-            'max-col': sheets.colCount,
-        });
-
-        // Get all cells in the sheet, excluding the header row
-        const cells = await promisify(sheets.getCells)({
-            'min-row': 2,
-            'max-row': sheets.rowCount,
-            'min-col': 1,
-            'max-col': sheets.colCount,
-        });
-
-        // Create an array to hold the rows of data
-        const rows = [];
-
-        // Iterate over the cells in the sheet and create an object for each row
-        let currentRow = 0;
-        cells.forEach(cell => {
-            // If we have reached a new row, create a new object to hold the data for that row
-            if (cell.row !== currentRow) {
-                currentRow = cell.row;
-                rows.push({});
-            }
-
-            // Add the cell value to the object for the current row
-            rows[rows.length - 1][headers[cell.col - 1].value] = cell.value;
-        });
-
-        return {headers, rows};
-    } catch (err) {
-        // Handle any errors that occurred
-        console.error(err);
-    }
+    // Convert the rows into an array of objects, using the headers as keys
+    return rows.map((row) =>
+        row.reduce((obj, cell, i) => {
+            obj[headers[i]] = cell;
+            return obj;
+        }, {})
+    );
 }
 
 const plugin = () => {
-    getRowsAsObjects()
-    .then(r => {
-        console.log(r);
+    getRowsFromSpreadsheet("1geHkSlE6e351LS_bMFGMIUBEOZO-HTb0wOS90X1jAp0", "AIzaSyA1otn2KKfYB3Svdfv30BhgJHPpWjVVrvw")
+    .then((rows) => {
+        console.log(rows);
     })
 }
 
-export default plugin
+export default plugin;
