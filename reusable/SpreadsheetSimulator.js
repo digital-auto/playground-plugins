0b576d7b96c2308fbcfd9b0118bcf9dbb66574fd
louis-jackman <77608615+louis-jackman@users.noreply.github.com>
2022-12-02 08:07:02 -0600

    Update SpreadsheetSimulator.js

--- reusable/SpreadsheetSimulator.js
+++ reusable/SpreadsheetSimulator.js
@@ -1,15 +1,16 @@
-async function getRowsFromSpreadsheet(spreadsheetId, apiKey) {
-    // Fetch the data from the Google Sheets API
+import SimulatorPlugins from "./SimulatorPlugins.js"
+
+async function fetchRowsFromSpreadsheet(spreadsheetId, apiKey) {
+    // Set the range to A1:Z1000
+    const range = "A1:Z1000";
+
+    // Fetch the rows from the Google Spreadsheet API
     const response = await fetch(
-        `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values/${encodeURIComponent("A1:Z1000")}?key=${apiKey}`
+        `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values/${range}?key=${encodeURIComponent(apiKey)}`
     );
-
     const json = await response.json();
-
     // Get the headers from the first row
-
     const headers = json.values[0];
-
     // Convert the remaining rows to an array of objects
     const rows = json.values.slice(1).map(row => {
         const rowObject = {};
@@ -19,15 +20,15 @@ async function getRowsFromSpreadsheet(spreadsheetId, apiKey) {
         return rowObject;
     });
 
-    return {headers, rows}
-
+    return rows;
 }
 
-const plugin = () => {
-    getRowsFromSpreadsheet("1geHkSlE6e351LS_bMFGMIUBEOZO-HTb0wOS90X1jAp0", "AIzaSyA1otn2KKfYB3Svdfv30BhgJHPpWjVVrvw")
-        .then(({headers, rows}) => {
-            console.log(headers, rows);
-        })
+
+const plugin = ({simulator}) => {
+    fetchRowsFromSpreadsheet("1geHkSlE6e351LS_bMFGMIUBEOZO-HTb0wOS90X1jAp0", "AIzaSyA1otn2KKfYB3Svdfv30BhgJHPpWjVVrvw")
+    .then((rows) => {
+        SimulatorPlugins(rows, simulator)
+    })
 }
 
 export default plugin;
