708e1a9dd831d686041c283aedcefc6a08b2067d
Nisanth6238R <120204892+Nisanth6238R@users.noreply.github.com>
2022-12-12 22:07:15 +0100

    Update Driverdoormonitoring.js

--- Driverdoormonitoring.js
+++ Driverdoormonitoring.js
@@ -43,7 +43,7 @@ const Driverdoormonitoring = ({widgets, vehicle}) => {
             if (LeftDoor = false) {
                 message += "\nThe CargoSpace Left Door is open\n\n"
             }
-            if (RightDoor == true) {
+            if (RightDoor = true) {
                 message += "The CargoSpace Left Door is open"
             }
             printNotification(message)
