52f05c9d217e1af771f71d2ab5857914d6bdd8e2
ChandhiniiBharathidasan <118307017+ChandhiniiBharathidasan@users.noreply.github.com>
2022-12-12 20:00:57 +0530

    Add files via upload

---
+++ Driverdoormonitoring.js
@@ -0,0 +1,75 @@
+import MobileNotifications from "./reusable/MobileNotifications.js"
+import GoogleMapsFromSignal from "./reusable/GoogleMapsFromSignal.js"
+import LineChart from "./reusable/LineChart.js"
+import SignalPills from "./reusable/SignalPills.js"
+import SignalTile from "./reusable/SignalTile.js"
+const Driverdoormonitoring = ({widgets, vehicle}) => {
+ const Proximity = {
+        signal: "Vehicle.Driver.ProximityToVehicle",
+        label: "PROX",
+        icon: "person",
+        suffix: "s"
+    }
+widgets.register(
+        "Proximity",
+        SignalPills(
+            Proximity,
+            vehicle
+        )
+    )
+   widgets.register(
+        "GoogleMapDirections",
+        GoogleMapsFromSignal(
+            [
+                {
+                    "lat": 48.149497,
+                    "lng": 11.523194
+                },
+                {
+                    "lat": 50.445168,
+                    "lng": 11.020569
+                },
+            ],
+            vehicle,
+            { iterate: true }
+        )
+    )
+
+  widgets.register("DriverProximityToVehicle", LineChart([
+       {
+                    signal: "Vehicle.Driver.ProximityToVehicle",
+                    suffix: " C",
+                    color: "yellow"
+                }
+    ], vehicle))
+  
+  
+  
+  widgets.register("MobileNotifications", (box) => {
+        const {printNotification} = MobileNotifications({box})
+        const intervalId = setInterval(async () => {
+            const [drivervehicledistance] = [
+              await vehicle.Vehicle.Driver.ProximityToVehicle.get()
+            ]
+            let message = ""
+            if (drivervehicledistance <= 10) {
+                message += "\nSafe driver vehicle distance\n\n"
+            }
+            if (drivervehicledistance >= 20) {
+                message += "Driver vehicle distance exceed the safe distance"
+            }
+            printNotification(message)
+        }, 300)
+
+        const iteratorIntervalidId = setInterval(async () => {
+            await vehicle.Next.get()
+        }, 3000)
+        
+        return ( ) => {
+            clearInterval(intervalId)
+            clearInterval(iteratorIntervalidId)
+        }
+    })
+}
+
+export default Driverdoormonitoring
