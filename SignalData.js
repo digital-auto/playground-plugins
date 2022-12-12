15c74450fe7621da7fdef175c311d1425a468d7b
Nisanth6238R <120204892+Nisanth6238R@users.noreply.github.com>
2022-12-12 17:17:37 +0100

    Update SignalData.js

--- SignalData.js
+++ SignalData.js
@@ -18,7 +18,7 @@ const plugin = ({ widgets, vehicle, simulator }) => {
 			"Vehicle.Trailer.Chassis.Axle.Row1.Wheel.Right.Brake.Temperature": 1,
 			"Vehicle.Trailer.Chassis.Axle.Row1.Temperature": 1,
 			"Vehicle.Trailer.Chassis.Axle.Row2.Temperature": 1,
-			"Vehicle.Trailer.CargoSpace.Door.Left.isOpen": false,
+			"Vehicle.Trailer.CargoSpace.Door.Left.isOpen": true,
 			"Vehicle.Trailer.CargoSpace.Door.Right.isOpen": false,
 			"Vehicle.Driver.IsPausing": false,
 			"Vehicle.Driver.Trip.CurrentSegment.AllowedDriveTime": 9,
