7bf443f586961902b266d552c55269ee9df7f9f7
Chris Cheng <54791922+ChrisCheng0217@users.noreply.github.com>
2022-12-22 10:43:49 +0100

    Update EVPowerOptimization.js

--- EVPowerOptimization.js
+++ EVPowerOptimization.js
@@ -36,7 +36,7 @@ const plugin = ({widgets, simulator, vehicle}) => {
     widgets.register("Table",StatusTable({
             apis:["Vehicle.Powertrain.TractionBattery.StateOfCharge.Current", "Vehicle.Powertrain.TractionBattery.AccumulatedConsumedEnergy", "Vehicle.Cabin.Infotainment.Media.Action", "Vehicle.Cabin.Lights.LightIntensity",	"Vehicle.TravelledDistance"],
             vehicle: vehicle,
-		    refresh: 1000         
+		    refresh: 500         
     }
 
     ))
