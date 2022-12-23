85ac835f39ccc0f9807ce764a88a87670f0054c6
Nisanth6238R <120204892+Nisanth6238R@users.noreply.github.com>
2022-12-23 21:11:56 +0100

    Update Driverdoormonitoring.js

--- Driverdoormonitoring.js
+++ Driverdoormonitoring.js
@@ -4,8 +4,39 @@ import LineChart from "./reusable/LineChart.js"
 import SignalPills from "./reusable/SignalPills.js"
 import SignalTile from "./reusable/SignalTile.js"
 import SignalWithMedia from "./reusable/SignalWithMedia.js"
+import SimulatorPlugins from "./reusable/SimulatorPlugins.js"
 
-const Driverdoormonitoring = ({widgets, vehicle}) => {
+async function fetchRowsFromSpreadsheet(spreadsheetId, apiKey) {
+    // Set the range to A1:Z1000
+    const range = "A1:Z1000";
+
+    // Fetch the rows from the Google Spreadsheet API
+    const response = await fetch(
+        `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values/${range}?key=${encodeURIComponent(apiKey)}`
+    );
+    const json = await response.json();
+    // Get the headers from the first row
+    const headers = json.values[0];
+    // Convert the remaining rows to an array of objects
+    const rows = json.values.slice(1).map(row => {
+        const rowObject = {};
+        for (let i = 0; i < row.length; i++) {
+            rowObject[headers[i]] = row[i];
+        }
+        return rowObject;
+    });
+
+    return rows;
+}
+
+
+const Driverdoormonitoring = ({widgets, vehicle, simulator}) => {
+ 
+    fetchRowsFromSpreadsheet("1P1YrDvAf_9MEGolYuZe8oz8NxNpvku8UB-nSa-gIIRU", "AIzaSyD8WaOWN38h1SynN7Ua0S9T5mSe_UDnUKo")
+    .then((rows) => {
+        SimulatorPlugins(rows, simulator)
+        console.log(rows)
+    })
  
    widgets.register(
         "GoogleMapDirections",
