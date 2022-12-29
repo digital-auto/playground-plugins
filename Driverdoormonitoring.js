5783d490fe859a94a1227d43d12e18b00aecbbb9
Nisanth6238R <120204892+Nisanth6238R@users.noreply.github.com>
2022-12-29 09:39:34 +0100

    Update Driverdoormonitoring.js

--- Driverdoormonitoring.js
+++ Driverdoormonitoring.js
@@ -121,20 +121,7 @@ widgets.register(
             },
         }, vehicle)
      )
-  let sim_function;
-       simulator("Vehicle.Speed", "subscribe", async ({func, args}) => {
-		sim_function = args[0]
-		console.log("print func", args[0])
-	})
-
-	return {
-		start_simulation : (time) => {
-			sim_intervalId = setInterval(async () => {
-				await vehicle.Next.get()
-				sim_function()
-			}, time)
-		}
-	}	
+  	
   let mobileNotifications = null;
           widgets.register("Mobile", (box) => {
                 ({printNotification: mobileNotifications} = MobileNotifications({
