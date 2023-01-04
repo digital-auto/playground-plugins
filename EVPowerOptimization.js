d6ce51fe73f049343b2ab22616fee69a5c990eff
Chris Cheng <54791922+ChrisCheng0217@users.noreply.github.com>
2023-01-04 12:59:32 +0100

    Update the wishlist VSS in line chart
    Vehicle.PowerOptimizationMode

--- EVPowerOptimization.js
+++ EVPowerOptimization.js
@@ -69,7 +69,7 @@ const plugin = ({widgets, simulator, vehicle}) => {
     }
 
     widgets.register("Table",StatusTable({
-            apis:["Vehicle.Powertrain.TractionBattery.StateOfCharge.Current", "Vehicle.Powertrain.TractionBattery.AccumulatedConsumedEnergy", "Vehicle.Cabin.Infotainment.Media.Action", "Vehicle.Cabin.Lights.LightIntensity",	"Vehicle.TravelledDistance", "Vehicle.CurrentLocation.Longitude","Vehicle.CurrentLocation.Latitude"],
+            apis:["Vehicle.PowerOptimizationMode","Vehicle.Powertrain.TractionBattery.StateOfCharge.Current", "Vehicle.Powertrain.TractionBattery.AccumulatedConsumedEnergy", "Vehicle.Cabin.Infotainment.Media.Action", "Vehicle.Cabin.Lights.LightIntensity",	"Vehicle.TravelledDistance", "Vehicle.CurrentLocation.Longitude","Vehicle.CurrentLocation.Latitude"],
             vehicle: vehicle,
 		    refresh: 800         
     }))
