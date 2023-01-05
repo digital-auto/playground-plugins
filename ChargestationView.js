ee8362131ef49ddafe85cdb0bdfe04fc6ab73f54
louis-jackman <77608615+louis-jackman@users.noreply.github.com>
2023-01-05 13:17:20 -0600

    Update ChargestationView.js

---
+++ ChargestationView.js
@@ -0,0 +1,66 @@
+const plugin = ({simulator, widgets}) => {
+    // Get the query string value of "chargestationId" with URLSearchParams
+    const params = new URLSearchParams(window.location.search)
+    const chargestationId = params.get('chargestationId')
+
+    const currentSignalValues = {
+        "Chargestation.ID": chargestationId,
+        "Chargestation.MaxCurrent": 0,
+        "Chargestation.MaxVoltage": 0,
+        "Chargestation.ChargingVehicleID": 0,
+        "Chargestation.Location.Latitude": 0,
+        "Chargestation.Location.Longitude": 0
+    }
+
+    // Simulators for each signal, that return the current value of the signal
+    for (const signal in currentSignalValues) {
+        simulator(signal, "get", async () => {
+            return currentSignalValues[signal]
+        })
+    }
+    
+
+    const updateChargestation = async () => {
+        if (!chargestationId) {
+            return
+        }
+        const response = await fetch(`https://evfleetsim.onrender.com/chargestation/${chargestationId}`)
+        const fleetJson = await response.json()
+        for (const signal in currentSignalValues) {
+            currentSignalValues[signal] = fleetJson[signal]
+        }
+    }
+
+    setInterval(() => {
+        updateChargestation()
+    }, 5000)
+
+    updateChargestation()
+
+    // Register a widget that renders a map with a marker with the chargestation's location
+    widgets.register("ChargestationMap", (box) => {
+        const container = document.createElement("div")
+        container.setAttribute("style", `display:flex; height: 100%; width: 100%;`)
+        box.injectNode(container)
+
+        const map = new box.window.google.maps.Map(container, {
+            zoom: 6.3,
+            center: {
+                lat: currentSignalValues["Chargestation.Location.Latitude"],
+                lng: currentSignalValues["Chargestation.Location.Longitude"]
+            }
+        });
+
+        const marker = new box.window.google.maps.Marker({
+            position: {
+                lat: currentSignalValues["Chargestation.Location.Latitude"],
+                lng: currentSignalValues["Chargestation.Location.Longitude"]
+            },
+            map: map
+        })
+        
+    })
+
+}
+
+export default plugin
