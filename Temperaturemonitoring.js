5f65e618983fd0606d55e00942c92aafb90661cd
ChandhiniiBharathidasan <118307017+ChandhiniiBharathidasan@users.noreply.github.com>
2023-01-11 17:20:57 +0530

    Update Temperaturemonitoring.js

--- Temperaturemonitoring.js
+++ Temperaturemonitoring.js
@@ -1,5 +1,5 @@
 import SimulatorPlugins from "./reusable/SimulatorPlugins.js"
-
+import { PLUGINS_APIKEY } from "./reusable/apikey.js";
 import MobileNotifications from "./reusable/MobileNotifications.js"
 import GoogleMapsFromSignal from "./reusable/GoogleMapsFromSignal.js"
 import LineChart from "./reusable/LineChart.js"
@@ -30,7 +30,7 @@ async function fetchRowsFromSpreadsheet(spreadsheetId, apiKey) {
 
 const plugin = ({widgets, simulator, vehicle}) => {
   
-    fetchRowsFromSpreadsheet("1vcrl5yRyMiAdsH1eIakfuHxocnYu6rgs5O-QHxnznj4", "AIzaSyBpMUJezbwUYARDHxFIR0a7h4yxh2v1dwI")
+    fetchRowsFromSpreadsheet("1vcrl5yRyMiAdsH1eIakfuHxocnYu6rgs5O-QHxnznj4", PLUGINS_APIKEY)
     .then((rows) => {
         SimulatorPlugins(rows, simulator)
         console.log(rows)
