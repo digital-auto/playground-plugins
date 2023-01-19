5daf92ec906130cda46b841f3f3564c2b35071c0
piyush-bajaj <bajaj.piyush.1995@gmail.com>
2023-01-19 22:25:54 +0100

    simulator call removed

--- AntiKinetosis.js
+++ AntiKinetosis.js
@@ -49,15 +49,7 @@ const plugin = ({widgets, simulator, vehicle}) => {
 		})
 	}
 
-	simulator("Vehicle.CurrentLocation.Latitude", "get", async ({prevReturnValue}) => {
-		return parseFloat(prevReturnValue * (180 / Math.PI))
-	})
-	simulator("Vehicle.CurrentLocation.Longitude", "get", async ({prevReturnValue}) => {
-		return parseFloat(prevReturnValue * (180 / Math.PI))
-	})
-
 	const updateSimulation = async () => {
-		
 		let score = await vehicle.Passenger.KinetosisScore.get()
 
 		scoreFrame.querySelector("#score .text").textContent = parseFloat(score).toFixed(2) + "%"
