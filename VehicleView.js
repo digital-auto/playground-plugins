6fbf5ff8b799ed69157f56dbd1ad9764a492245b
louis-jackman <77608615+louis-jackman@users.noreply.github.com>
2023-01-22 05:29:49 -0600

    Update VehicleView.js

--- VehicleView.js
+++ VehicleView.js
@@ -95,7 +95,7 @@ const plugin = ({simulator, widgets, modelObjectCreator}) => {
             const start = new box.window.google.maps.LatLng(path[0].lat, path[0].lng);
             const end = new box.window.google.maps.LatLng(path[1].lat, path[1].lng);        
 
-            console.log(path)
+            console.log(box.window)
 
             // const directionsService = new box.window.google.maps.DirectionsService();
             // directionsService
