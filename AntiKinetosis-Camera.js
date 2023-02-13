c1c0f2a20ec3ba9c56566107e58ccf54903a6a9f
piyush-bajaj <bajaj.piyush.1995@gmail.com>
2023-02-13 10:49:56 +0100

    css improvements

--- AntiKinetosis-Camera.js
+++ AntiKinetosis-Camera.js
@@ -23,23 +23,23 @@ async function imageUpload(image) {
 
 const plugin = ({widgets, simulator, vehicle}) => {
 
+    let webcam_message = 'Turn on Webcam'
+
     const container = document.createElement('div')
     container.innerHTML = 
     `
-    <div id="image-show">
+    <div id="image" style="display:none">
         <img id="output" width="100%"/>
     </div>
-    <div id="image-hide">
-        <p>
-            Capture the image or Upload the image
-        </p>
+    <div id="video" style="display:none">
+        <video id="webcam-video" playsinline autoplay> </video>
     </div>
     <div class="btn btn-color" style="display:flex; position:absolute; width: 100%; bottom: 10px; opacity:50%; align-items:center; align-content:center; flex-direction:row; justify-content:center">
         <button id="upload-btn" style="background-color: rgb(104 130 158);padding: 10px 24px;cursor: pointer;float: left;margin:2px;border-radius:5px;font-size:1em;font-family:Lato;color: rgb(255, 255, 227);border:0px">
             Upload
         </button>
         <button id="capture-btn" style="background-color: rgb(104 130 158);padding: 10px 24px;cursor: pointer;float: left;margin:2px;border-radius:5px;font-size:1em;font-family:Lato;color: rgb(255, 255, 227);border:0px">
-            Capture
+            ${webcam_message}
         </button>
         <input id="upload" type="file" accept="image/*" style="display:none">
     </div>
@@ -48,6 +48,19 @@ const plugin = ({widgets, simulator, vehicle}) => {
     const upload_btn = container.querySelector("#upload-btn")
     upload_btn.onclick = () => {
         container.querySelector("#upload").click()
+    }
+
+    const capture_btn = container.querySelector("#capture_btn")
+    capture_btn.onclick = () => {
+        if(webcam_message === "Turn on Webcam") {
+            console.log('turn on webcam')
+            webcam_message = "Capture"
+        }
+        else {
+            // const image = container.querySelector('#output');
+            // image.src = URL.createObjectURL(event.target.files[0]);
+            console.log("capture image")
+        }
     }
 
     const upload = container.querySelector("#upload")
