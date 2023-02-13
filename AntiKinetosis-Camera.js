444d7c7cf5aa89c48be4fd336ccc502d24be9ea1
piyush-bajaj <bajaj.piyush.1995@gmail.com>
2023-02-13 11:31:53 +0100

    cam image

--- AntiKinetosis-Camera.js
+++ AntiKinetosis-Camera.js
@@ -34,7 +34,7 @@ const plugin = ({widgets, simulator, vehicle}) => {
     <div id="video" style="display:none">
         <video id="webcam-video" playsinline autoplay> </video>
     </div>
-    <div id="image_canvas" style="display:none">
+    <div id="video_canvas" style="display:none">
         <canvas style="border:solid 1px #ddd;background-color:white;" id="canvas" width="475" height="475"></canvas>    
     </div>
     <div class="btn btn-color" style="display:flex; position:absolute; width: 100%; bottom: 10px; opacity:50%; align-items:center; align-content:center; flex-direction:row; justify-content:center">
@@ -56,7 +56,7 @@ const plugin = ({widgets, simulator, vehicle}) => {
     const capture_btn = container.querySelector("#capture-btn")
     capture_btn.onclick = () => {
         const video = container.querySelector("#webcam-video")
-        
+
         if(webcam_message === "Webcam") {
             webcam_message = "Capture"
             container.querySelector("#capture-btn").innerText = webcam_message
@@ -76,19 +76,38 @@ const plugin = ({widgets, simulator, vehicle}) => {
                         console.log("Something went wrong!");  
                     });  
             }
+            container.querySelector("#image").style = "display: none"
+            container.querySelector("#video").style = "display: block"
         }
         else {
-            // const image = container.querySelector('#output');
-            // image.src = URL.createObjectURL(event.target.files[0]);
-            console.log("capture image")
-            const canvas = document.getElementById('canvas');  
-            const context = canvas.getContext('2d');  
+            const image = container.querySelector('#output');
+            const canvas = document.createElement('canvas');
+            //const imgWidth1 = rawVideoFrame.videoWidth;
+            //const imgHeight1 = rawVideoFrame.videoHeight;
+            //canvas1.width = imgWidth1;
+            //canvas1.height = imgHeight1;
+            const context = canvas.getContext('2d');
+            context.drawImage(video, 0, 0);
     
-            // Capture the image into canvas from Webcam streaming Video element  
-            context.drawImage(video, 0, 0);  
+            image.setAttribute("crossorigin", "anonymous")
+            const data = canvas.toDataURL("image/png");
+            photo1.setAttribute("src", data);
+            container.querySelector("#image").style = "display: block"
+            container.querySelector("#video").style = "display: none"
 
         }
     }
+
+    // function stop() {  
+    //     var stream = video.srcObject;  
+    //     var tracks = stream.getTracks();  
+  
+    //     for (var i = 0; i < tracks.length; i++) {  
+    //         var track = tracks[i];  
+    //         track.stop();  
+    //     }  
+    //     video.srcObject = null;  
+    // }
 
     const upload = container.querySelector("#upload")
     upload.onchange = (event) => {
