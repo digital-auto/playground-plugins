c0f6cf0a8c6a8874fc49bfa9e3f9bcd15740fb50
piyush-bajaj <bajaj.piyush.1995@gmail.com>
2023-03-13 12:06:24 +0100

    video play

--- WalletDetection.js
+++ WalletDetection.js
@@ -81,9 +81,9 @@ const plugin = ({widgets, simulator, vehicle}) => {
         <div id="video" style="display:none; width:100%; height:100%">
             <video id="raw-video" width="100%" height="100%">
             </video>
-            <div style="width:3em;cursor: pointer;position:absolute;bottom:45%;left:45%"" id="play-btn">
+            <!-- <div style="width:3em;cursor: pointer;position:absolute;bottom:45%;left:45%"" id="play-btn">
 				<img src="https://firebasestorage.googleapis.com/v0/b/digital-auto.appspot.com/o/media%2Fplay.svg?alt=media&token=4f68e20d-5c11-4e2c-9ae3-7f44ebdd0416" alt="play" style="filter: invert(100%);">
-			</div>
+			</div> -->
         </div>
         <div class="btn btn-color" style="display:flex; position:absolute; width: 100%; bottom: 10px; opacity:50%; align-items:center; align-content:center; flex-direction:row; justify-content:center">
             <button id="upload-btn" style="background-color: rgb(104 130 158);padding: 10px 24px;cursor: pointer;float: left;margin:2px;border-radius:5px;font-size:1em;font-family:Lato;color: rgb(255, 255, 227);border:0px">
@@ -143,10 +143,10 @@ const plugin = ({widgets, simulator, vehicle}) => {
             console.log(res)
         }
 
-        const play_btn = container.querySelector("#play-btn")
+        /* const play_btn = container.querySelector("#play-btn")
         play_btn.onclick = () => {
             container.querySelector("#raw-video").play();            
-        }
+        } */
 
         box.injectNode(container)
 
