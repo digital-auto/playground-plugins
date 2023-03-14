99e87bf56e56a13ab90920b8a85e422e113ad020
piyush-bajaj <bajaj.piyush.1995@gmail.com>
2023-03-14 11:22:13 +0100

    css

--- AntiKinetosis-Camera.js
+++ AntiKinetosis-Camera.js
@@ -209,7 +209,7 @@ const plugin = ({widgets, simulator, vehicle}) => {
 		</div>
 		<div class="label" style="width:100%;position:relative;margin-top:10px;">Back Seat passengers: </div>
 		<div id="passengers" style="position:relative;margin-top:5px;width:100%;">        
-			<div class="selections" style="display:block;position:relative;justify-content:center">
+			<div class="selections" style="display:flex;position:relative;justify-content:center">
 				<!-- <div class="btn-group gender" style="margin:5px;display:grid">
 					<button id="gender_male" style="background-color: rgb(104 130 158);padding: 10px 24px;cursor: pointer;float: left;margin:2px;border-radius:5px;font-size:1em;font-family:Lato;color: rgb(255, 255, 227);border:0px">
 					Male
@@ -237,7 +237,7 @@ const plugin = ({widgets, simulator, vehicle}) => {
 					<div><strong>Age : </strong></div>
 					<input type="number" id="age-input" style="width:50%" min="0" max="200" value="15"/>
 				</div> -->
-				<div style="margin:5px; display:block">
+				<div style="margin:5px;">
 					<table>
 						<tr>
 							<th></th>
