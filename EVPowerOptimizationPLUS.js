dd8bdb259406eaecb9969bddb38d01e8c3724733
Nhan <nhan.luongnguyen@vn.bosch.com>
2023-05-12 15:39:03 +0700

    EVPowerOptimizationPLUS: Change red wind img to blue wind img, fix time 02

--- EVPowerOptimizationPLUS.js
+++ EVPowerOptimizationPLUS.js
@@ -80,7 +80,7 @@ const plugin = ({widgets, simulator, vehicle}) => {
         if( inf_light === 100 && temp === 15 && fan_speed === 100 && media_volume === 100) {
             IVIAnimationFrame.querySelector("#mainText").innerHTML = `Estimated travel range: ${(bat_soc*4.03).toFixed(2)} km <br> Distance to the nearest charging station: ${(450 - trvl_dist).toFixed(2)} km <br> Media volume: 100 <br> Interior Light System: Maximum light`;
             HVACAnimationFrame.querySelector("#show").innerHTML = "Current air conditioner temperature: 15<br>Fan speed: 100";
-            HVACAnimationFrame.querySelector("#wind").setAttribute("src", "https://firebasestorage.googleapis.com/v0/b/digital-auto.appspot.com/o/media%2Fevpoweroptimization%2Fhvac%2Fbig.gif?alt=media&token=4587f1ef-a9e5-45f5-b3cd-c5a617a65811");
+            HVACAnimationFrame.querySelector("#wind").setAttribute("src", "https://firebasestorage.googleapis.com/v0/b/digital-auto.appspot.com/o/media%2Fblue%20air.gif?alt=media&token=6a00f612-649e-4587-9b46-0be192588088");
             IVIAnimationFrame.querySelector("#btnImg").setAttribute("src","https://firebasestorage.googleapis.com/v0/b/digital-auto.appspot.com/o/media%2Fevpoweroptimization%2Fivi%2Fpause.png?alt=media&token=8d615884-44aa-4bcb-93bc-49a0c3bb7958")
             IVIAnimationFrame.querySelector("#songName").style.animationPlayState = "running";
             IVIAnimationFrame.querySelector("#modelImg").style.animationPlayState = "running";
@@ -90,7 +90,7 @@ const plugin = ({widgets, simulator, vehicle}) => {
         else if(inf_light === 100 && temp === 15 && fan_speed === 100 && media_volume === 70) {
             IVIAnimationFrame.querySelector("#mainText").innerHTML = `Estimated travel range: ${(bat_soc*4.20).toFixed(2)} km <br> Distance to the nearest charging station: ${(450 - trvl_dist).toFixed(2)} km <br> Media volume: 70 <br> Interior Light System: Maximum light`;
             HVACAnimationFrame.querySelector("#show").innerHTML = "Current air conditioner temperature: 15<br>Fan speed: 100";
-            HVACAnimationFrame.querySelector("#wind").setAttribute("src", "https://firebasestorage.googleapis.com/v0/b/digital-auto.appspot.com/o/media%2Fevpoweroptimization%2Fhvac%2Fbig.gif?alt=media&token=4587f1ef-a9e5-45f5-b3cd-c5a617a65811");
+            HVACAnimationFrame.querySelector("#wind").setAttribute("src", "https://firebasestorage.googleapis.com/v0/b/digital-auto.appspot.com/o/media%2Fblue%20air.gif?alt=media&token=6a00f612-649e-4587-9b46-0be192588088");
             IVIAnimationFrame.querySelector("#btnImg").setAttribute("src","https://firebasestorage.googleapis.com/v0/b/digital-auto.appspot.com/o/media%2Fevpoweroptimization%2Fivi%2Fpause.png?alt=media&token=8d615884-44aa-4bcb-93bc-49a0c3bb7958")
             IVIAnimationFrame.querySelector("#songName").style.animationPlayState = "running";
             IVIAnimationFrame.querySelector("#modelImg").style.animationPlayState = "running";
@@ -100,7 +100,7 @@ const plugin = ({widgets, simulator, vehicle}) => {
         else if(inf_light === 100 && temp === 15 && fan_speed === 100 && media_volume === 40) {
             IVIAnimationFrame.querySelector("#mainText").innerHTML = `Estimated travel range: ${(bat_soc*4.23).toFixed(2)} km <br> Distance to the nearest charging station: ${(450 - trvl_dist).toFixed(2)} km <br> Media volume: 40 <br> Interior Light System: Maximum light`;
             HVACAnimationFrame.querySelector("#show").innerHTML = "Current air conditioner temperature: 15<br>Fan speed: 100";
-            HVACAnimationFrame.querySelector("#wind").setAttribute("src", "https://firebasestorage.googleapis.com/v0/b/digital-auto.appspot.com/o/media%2Fevpoweroptimization%2Fhvac%2Fbig.gif?alt=media&token=4587f1ef-a9e5-45f5-b3cd-c5a617a65811");
+            HVACAnimationFrame.querySelector("#wind").setAttribute("src", "https://firebasestorage.googleapis.com/v0/b/digital-auto.appspot.com/o/media%2Fblue%20air.gif?alt=media&token=6a00f612-649e-4587-9b46-0be192588088");
             IVIAnimationFrame.querySelector("#btnImg").setAttribute("src","https://firebasestorage.googleapis.com/v0/b/digital-auto.appspot.com/o/media%2Fevpoweroptimization%2Fivi%2Fpause.png?alt=media&token=8d615884-44aa-4bcb-93bc-49a0c3bb7958")
             IVIAnimationFrame.querySelector("#songName").style.animationPlayState = "running";
             IVIAnimationFrame.querySelector("#modelImg").style.animationPlayState = "running";
@@ -109,7 +109,7 @@ const plugin = ({widgets, simulator, vehicle}) => {
         else if(inf_light === 100 && temp === 15 && fan_speed === 100 && media_volume === 10) {
             IVIAnimationFrame.querySelector("#mainText").innerHTML = `Estimated travel range: ${(bat_soc*4.26).toFixed(2)} km <br> Distance to the nearest charging station: ${(450 - trvl_dist).toFixed(2)} km <br> Media volume: 10 <br> Interior Light System: Maximum light`;
             HVACAnimationFrame.querySelector("#show").innerHTML = "Current air conditioner temperature: 15<br>Fan speed: 100";
-            HVACAnimationFrame.querySelector("#wind").setAttribute("src", "https://firebasestorage.googleapis.com/v0/b/digital-auto.appspot.com/o/media%2Fevpoweroptimization%2Fhvac%2Fbig.gif?alt=media&token=4587f1ef-a9e5-45f5-b3cd-c5a617a65811");
+            HVACAnimationFrame.querySelector("#wind").setAttribute("src", "https://firebasestorage.googleapis.com/v0/b/digital-auto.appspot.com/o/media%2Fblue%20air.gif?alt=media&token=6a00f612-649e-4587-9b46-0be192588088");
             IVIAnimationFrame.querySelector("#btnImg").setAttribute("src","https://firebasestorage.googleapis.com/v0/b/digital-auto.appspot.com/o/media%2Fevpoweroptimization%2Fivi%2Fpause.png?alt=media&token=8d615884-44aa-4bcb-93bc-49a0c3bb7958")
             IVIAnimationFrame.querySelector("#songName").style.animationPlayState = "running";
             IVIAnimationFrame.querySelector("#modelImg").style.animationPlayState = "running";
@@ -118,7 +118,7 @@ const plugin = ({widgets, simulator, vehicle}) => {
         else if(inf_light === 70 && temp === 15 && fan_speed === 100 && media_volume === 10) {
             IVIAnimationFrame.querySelector("#mainText").innerHTML = `Estimated travel range: ${(bat_soc*4.26).toFixed(2)} km <br> Distance to the nearest charging station: ${(450 - trvl_dist).toFixed(2)} km <br> Media volume: 10 <br> Interior Light System: Medium light`;
             HVACAnimationFrame.querySelector("#show").innerHTML = "Current air conditioner temperature: 15<br>Fan speed: 100";
-            HVACAnimationFrame.querySelector("#wind").setAttribute("src", "https://firebasestorage.googleapis.com/v0/b/digital-auto.appspot.com/o/media%2Fevpoweroptimization%2Fhvac%2Fbig.gif?alt=media&token=4587f1ef-a9e5-45f5-b3cd-c5a617a65811");
+            HVACAnimationFrame.querySelector("#wind").setAttribute("src", "https://firebasestorage.googleapis.com/v0/b/digital-auto.appspot.com/o/media%2Fblue%20air.gif?alt=media&token=6a00f612-649e-4587-9b46-0be192588088");
             IVIAnimationFrame.querySelector("#btnImg").setAttribute("src","https://firebasestorage.googleapis.com/v0/b/digital-auto.appspot.com/o/media%2Fevpoweroptimization%2Fivi%2Fpause.png?alt=media&token=8d615884-44aa-4bcb-93bc-49a0c3bb7958")
             IVIAnimationFrame.querySelector("#songName").style.animationPlayState = "running";
             IVIAnimationFrame.querySelector("#modelImg").style.animationPlayState = "running";
@@ -127,7 +127,7 @@ const plugin = ({widgets, simulator, vehicle}) => {
         else if(inf_light === 40 && temp === 15 && fan_speed === 100 && media_volume === 10) {
             IVIAnimationFrame.querySelector("#mainText").innerHTML = `Estimated travel range: ${(bat_soc*4.27).toFixed(2)} km <br> Distance to the nearest charging station: ${(450 - trvl_dist).toFixed(2)} km <br> Media volume: 10 <br> Interior Light System: Weak light`;
             HVACAnimationFrame.querySelector("#show").innerHTML = "Current air conditioner temperature: 15<br>Fan speed: 100";
-            HVACAnimationFrame.querySelector("#wind").setAttribute("src", "https://firebasestorage.googleapis.com/v0/b/digital-auto.appspot.com/o/media%2Fevpoweroptimization%2Fhvac%2Fbig.gif?alt=media&token=4587f1ef-a9e5-45f5-b3cd-c5a617a65811");
+            HVACAnimationFrame.querySelector("#wind").setAttribute("src", "https://firebasestorage.googleapis.com/v0/b/digital-auto.appspot.com/o/media%2Fblue%20air.gif?alt=media&token=6a00f612-649e-4587-9b46-0be192588088");
             IVIAnimationFrame.querySelector("#btnImg").setAttribute("src","https://firebasestorage.googleapis.com/v0/b/digital-auto.appspot.com/o/media%2Fevpoweroptimization%2Fivi%2Fpause.png?alt=media&token=8d615884-44aa-4bcb-93bc-49a0c3bb7958")
             IVIAnimationFrame.querySelector("#songName").style.animationPlayState = "running";
             IVIAnimationFrame.querySelector("#modelImg").style.animationPlayState = "running";
@@ -136,7 +136,7 @@ const plugin = ({widgets, simulator, vehicle}) => {
         else if(inf_light === 40 && temp === 18 && fan_speed === 100 && media_volume === 10) {
             IVIAnimationFrame.querySelector("#mainText").innerHTML = `Estimated travel range: ${(bat_soc*4.31).toFixed(2)} km <br> Distance to the nearest charging station: ${(450 - trvl_dist).toFixed(2)} km <br> Media volume: 10 <br> Interior Light System: Weak light`;
             HVACAnimationFrame.querySelector("#show").innerHTML = "Current air conditioner temperature: 18<br>Fan speed: 100";
-            HVACAnimationFrame.querySelector("#wind").setAttribute("src", "https://firebasestorage.googleapis.com/v0/b/digital-auto.appspot.com/o/media%2Fevpoweroptimization%2Fhvac%2Fbig.gif?alt=media&token=4587f1ef-a9e5-45f5-b3cd-c5a617a65811");
+            HVACAnimationFrame.querySelector("#wind").setAttribute("src", "https://firebasestorage.googleapis.com/v0/b/digital-auto.appspot.com/o/media%2Fblue%20air.gif?alt=media&token=6a00f612-649e-4587-9b46-0be192588088");
             IVIAnimationFrame.querySelector("#btnImg").setAttribute("src","https://firebasestorage.googleapis.com/v0/b/digital-auto.appspot.com/o/media%2Fevpoweroptimization%2Fivi%2Fpause.png?alt=media&token=8d615884-44aa-4bcb-93bc-49a0c3bb7958")
             IVIAnimationFrame.querySelector("#songName").style.animationPlayState = "running";
             IVIAnimationFrame.querySelector("#modelImg").style.animationPlayState = "running";
