be52ee8d107c85952b512dbddd3a68a7dc1746d0
Chris Cheng <54791922+ChrisCheng0217@users.noreply.github.com>
2023-06-26 14:50:04 +0200

    Update WalletDetection.js
    Update the size of LandingAI Logo

--- WalletDetection.js
+++ WalletDetection.js
@@ -68,7 +68,7 @@ const plugin = ({widgets, simulator, vehicle}) => {
     widgets.register("Result", (box) => {
         container = document.createElement('div')
         container.innerHTML = `
-            <div style="width:100%;height:100%;position: relative">
+            <div style="width:50%;height:50%;position: relative">
                 <div id="resultRec" style="position:absolute;border: 2px solid red;top: 0;left:0;width:0;height:0;z-index:2;"></div>
                 <img id="resultImg" style="width:100%;height:100%;position:absolute;top:0;left:0;right:0;bottom:0;z-index:1;"
                     src="https://firebasestorage.googleapis.com/v0/b/digital-auto.appspot.com/o/media%2FLanding_AI_Logo_RGB_600.png?alt=media&token=9f6e445d-cf6d-4556-9240-4645a804b240"/> 
