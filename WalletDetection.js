fe684d346b0c5866e658f4c445a2cbf9ac0ebb1e
Chris Cheng <54791922+ChrisCheng0217@users.noreply.github.com>
2023-06-26 15:00:09 +0200

    Update WalletDetection.js

--- WalletDetection.js
+++ WalletDetection.js
@@ -65,19 +65,19 @@ const plugin = ({widgets, simulator, vehicle}) => {
     let imgWidth = 0;
     let imgHeight = 0;
 
-    widgets.register("Result", (box) => {
-        container = document.createElement('div')
-        container.innerHTML = `
-            <div style="width:50%;height:50%;position: relative">
-                <div id="resultRec" style="position:absolute;border: 2px solid red;top: 0;left:0;width:0;height:0;z-index:2;"></div>
-                <img id="resultImg" style="width:100%;height:100%;position:absolute;top:0;left:0;right:0;bottom:0;z-index:1;"
-                    src="https://firebasestorage.googleapis.com/v0/b/digital-auto.appspot.com/o/media%2FLanding_AI_Logo_RGB_600.png?alt=media&token=9f6e445d-cf6d-4556-9240-4645a804b240"/> 
-            </div>
-        `
-        resultImgDiv = container.querySelector("#resultImg")
-        resultRecDiv = container.querySelector("#resultRec")
-        box.injectNode(container)
-    })
+widgets.register("Result", (box) => {
+    const container = document.createElement('div');
+    container.innerHTML = `
+        <div style="width: 50%; height: 50%; display: flex; align-items: center; justify-content: center;">
+            <div id="resultRec" style="position: absolute; border: 2px solid red; top: 0; left: 0; width: 0; height: 0; z-index: 2;"></div>
+            <img id="resultImg" style="max-width: 100%; max-height: 100%; z-index: 1;"
+                src="https://firebasestorage.googleapis.com/v0/b/digital-auto.appspot.com/o/media%2FLanding_AI_Logo_RGB_600.png?alt=media&token=9f6e445d-cf6d-4556-9240-4645a804b240"/> 
+        </div>
+    `;
+    const resultImgDiv = container.querySelector("#resultImg");
+    const resultRecDiv = container.querySelector("#resultRec");
+    box.injectNode(container);
+});
 
     widgets.register("Video Panel", (box) => {
         container = document.createElement('div')
