78d6fdca146e4fac0fc808c362dad254a56116bc
NhanLuongBGSV <nhan.luongnguyen@vn.bosch.com>
2023-06-26 18:48:20 +0700

    Change EVpoweroptimizationPlus back to aiotapp.net

--- EVPowerOptimizationPLUS.js
+++ EVPowerOptimizationPLUS.js
@@ -31,8 +31,8 @@ async function fetchRowsFromSpreadsheet(spreadsheetId, apiKey) {
 
 const anysisSimulation = async (call, policy) => {
     const res = await fetch(
-        `https://app.digitalauto.tech/evpoweroptimization`, {
-        // `https://aiotapp.net/evpoweroptimization`, {
+        // `https://app.digitalauto.tech/evpoweroptimization`, {
+        `https://aiotapp.net/evpoweroptimization`, {
             method:'POST',
             mode: 'cors',
             cache: 'no-cache',
