1c7532315be8c1a700603d24c91a2f5af4b0e977
chris cheng <chris.cheng@ferdinand-steinbeis-institut.de>
2023-09-03 23:19:50 +0200

    test socket.io

---
+++ TestServerAPI.js
@@ -0,0 +1,73 @@
+const loadScript = (boxWindow, url) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const script = boxWindow.document.createElement("script");
+            script.defer = true;
+            script.referrerPolicy = "origin"
+
+            script.src = url;
+            boxWindow.document.head.appendChild(script);
+            script.addEventListener("load", () => resolve(undefined));
+        } catch (e) {
+            reject();
+        }
+    });
+}
+// const PROVIDER_ID = "PYTHON-CLIENT-SAMPLE"
+const PROVIDER_ID = "CHRIS-FSTI"
+
+const plugin = ({widgets, simulator, vehicle}) => {
+
+    widgets.register("Client", async (box) => {
+        await loadScript(box.window, `https://cdn.socket.io/4.6.0/socket.io.min.js`)
+        const socket = box.window.io("https://bridge.digitalauto.tech");
+
+        const onConnected = () => {
+            console.log("Io connected")
+            socket.emit("register_client", {
+                master_provider_id: PROVIDER_ID
+            })
+        }
+        const messageFromProvider = (payload) => {
+            if(payload.cmd == 'showSpeed') {
+                lblSpeed.innerText = payload.data
+            }
+        }
+        const onProviderReply = (payload) => {
+            lblSpeed.innerText = payload.result
+        }
+
+        socket.on("connect", onConnected);
+        socket.on('message_from_provider', messageFromProvider)
+        socket.on('provider_reply', onProviderReply)
+
+        const container = document.createElement("div");
+        container.setAttribute("style", `display:block; ;overflow:auto;padding: 20px;`);
+        container.innerHTML = `
+            <div style='margin-top: 10px;font-size:20px;'>
+                <div style='display:inline-block;width: 100px;'>Speed</div>
+                <div style='display:inline-block;font-weight: 700' id='lblSpeed'></div>
+            </div>
+            <div style='margin-top: 10px;'>
+                <div style='display:inline-block;font-weight: 700;padding: 8px 12px;background-color:#ABABAB;cursor:pointer;border-radius:4px;'
+                    id='btnStart'> Start</div>
+            </div>
+        `
+        let lblSpeed = container.querySelector("#lblSpeed")
+        let btnStart = container.querySelector("#btnStart")
+        btnStart.onclick = () => {
+            socket.emit("request_provider", {
+                to_provider_id: PROVIDER_ID,
+                cmd: "Start",
+                data: 1
+            })
+        }
+        box.injectNode(container);
+    })
+    return {
+        call_me: (name) => { 
+            return "Hello " + name + ", I am plugin."
+        }
+    }
+}
+export default plugin
