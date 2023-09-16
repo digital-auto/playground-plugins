fa0077f617f9fe79e4bddc26039f3e6529c8d6ca
Gharsallah <ismail.gharsallah@ferdinand-steinbeis-institut.de>
2023-09-17 01:44:02 +0200

    send vss to provider

--- EVPowerOptimizationPLUSDev.js
+++ EVPowerOptimizationPLUSDev.js
@@ -991,11 +991,11 @@ const handleWindowClose = async (e) => {
           }
           
           async function delayedGreeting() {
-            //if (policy!=11)
+            if (policy!=11)
             while(1){
             await updateSimulation()
-            await sleep(2000);
-            console.log("sleep");
+            await sleep(1000);
+            //console.log("sleep");
             
           }
         }
