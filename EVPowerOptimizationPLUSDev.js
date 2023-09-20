3131e884ed1a2f84e8695a6d97eee84da888ddb3
Nhan Luong <130432957+NhanLuongBGSV@users.noreply.github.com>
2023-09-20 13:45:41 +0700

    change query way

--- EVPowerOptimizationPLUSDev.js
+++ EVPowerOptimizationPLUSDev.js
@@ -1011,7 +1011,7 @@ const plugin = ({ widgets, simulator, vehicle }) => {
         }
 
         const renderActivePolicy = () => {
-            let policies =  document.querySelector("#pol")
+            let policies =  PolicyFrame.querySelectorAll("#pol")
             console.log(`policy ${policy} policies`, policies)
             if(policies) {
                 policies.forEach((pol) => {
