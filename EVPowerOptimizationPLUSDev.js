e421ea8984c84588523d3dbb490c940c2d58550b
Nhan Luong <130432957+NhanLuongBGSV@users.noreply.github.com>
2023-09-20 13:50:01 +0700

    render policy

--- EVPowerOptimizationPLUSDev.js
+++ EVPowerOptimizationPLUSDev.js
@@ -1016,9 +1016,9 @@ const plugin = ({ widgets, simulator, vehicle }) => {
             if(policies) {
                 policies.forEach((pol) => {
                     if(pol.id == 'pol'+policy) {
-                        pol.style.backgroundColor = "rgb(157 176 184)"
-                    } else {
                         pol.style.backgroundColor = "rgb(104 130 158)"
+                    } else {
+                        pol.style.backgroundColor = "rgb(157 176 184)"
                     }
                 })
             }
