996aa5f9f8845947f5aa02e22ba341751bb268f0
Gharsallah <ismail.gharsallah@ferdinand-steinbeis-institut.de>
2023-10-02 19:13:41 +0200

    Impliment the new API

--- AntiKinetosis-Camera.js
+++ AntiKinetosis-Camera.js
@@ -7,7 +7,7 @@ async function imageUpload(image) {
 	*/
 	
 
-    encodedImage = image.replace('data:image/jpeg;base64,', '')
+    let encodedImage = image.replace('data:image/jpeg;base64,', '')
 	console.log(JSON.stringify({encodedImage}));
 	const res = await fetch(
 		//`https://aiotapp.net/kinetosis/detectImage`, {
