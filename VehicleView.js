d133002e4971b8131c0c3dd69ce81e75ba8220bb
isma3il-gharsallah <ismail.gharsallah8@gmail.com>
2023-12-15 13:17:57 +0100

    Change destination and origin Positions

--- VehicleView.js
+++ VehicleView.js
@@ -112,12 +112,12 @@ const plugin = ({simulator, widgets, modelObjectCreator}) => {
         return GoogleMapsFromSignal(
             [
                 {
-                    lat: 49.1427,
-                    lng: 9.2109
+                    lat: 36.8065,
+                    lng: 10.1815
                 },
                 {
-                    lat: 50.1109,
-                    lng: 8.6821
+                    lat: 36.4513,
+                    lng: 10.7357
                 },
             ],
             vehicle,
