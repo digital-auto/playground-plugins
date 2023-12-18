02a621e74df1c52c7f6ef11d5155ec93e64e41f0
isma3il-gharsallah <ismail.gharsallah8@gmail.com>
2023-12-18 09:38:41 +0100

    Add chargers liste

--- VehicleView.js
+++ VehicleView.js
@@ -107,8 +107,6 @@ const plugin = ({simulator, widgets, modelObjectCreator}) => {
                 })
                 .catch((e) => console.log("Directions request failed due to " + e));    
             }, 0)
-
-
         })
         
 
@@ -130,6 +128,151 @@ const plugin = ({simulator, widgets, modelObjectCreator}) => {
 
     })
     //////////////// End test Maps ////////////
+    //Maps with markets/////
+    var marker; // move marker definition into the global scope
+var infowindow;
+var uniqueId = 1;
+var infoWindowcontent;
+var markers = [];
+
+function calculateAndDisplayRoute(directionsService, directionsRenderer) {
+  directionsService.route({
+      origin: {
+        query: document.getElementById('departure_address').value
+      },
+      destination: {
+        query: document.getElementById('arrival_address').value
+      },
+      travelMode: 'DRIVING'
+    },
+    function(response, status) {
+      if (status === 'OK') {
+        var point = response.routes[0].legs[0];
+        directionsRenderer.setDirections(response);
+
+        createMarker(response.routes[0].legs[0].start_location, "A", "start marker", directionsRenderer.getMap(), infowindow);
+        var lastLeg = response.routes[0].legs.length - 1;
+        createMarker(response.routes[0].legs[lastLeg].end_location, "B", "end marker", directionsRenderer.getMap(), infowindow);
+        if (marker && marker.setMap) // hide click marker when directions displayed
+          marker.setMap(null);
+      } else {
+        alert('Directions request failed due to ' + status);
+      }
+    });
+}
+
+function initMap_mobile() {
+  var directionsService = new google.maps.DirectionsService();
+  var directionsRenderer = new google.maps.DirectionsRenderer({
+    suppressMarkers: true
+  });
+  map = new google.maps.Map(document.getElementById('mobilemap'), {
+    mapTypeControl: false,
+    center: {
+      lat: 42.700000762939,
+      lng: 23.333299636841
+    },
+    zoom: 13
+  });
+  directionsRenderer.setMap(map);
+
+  google.maps.event.addListener(map, 'click', function(event) {
+    var geocoder = new google.maps.Geocoder();
+    geocoder.geocode({
+      'latLng': new google.maps.LatLng(event.latLng.lat(), event.latLng.lng())
+    }, function(results, status) {
+      //otherwise clicks twice
+      set_lat_long(event.latLng.lat(), event.latLng.lng(), results[0].formatted_address, directionsService, directionsRenderer);
+
+      if (uniqueId == 1) {
+        label = 'From Address';
+        infoWindowcontent = '<div class="ib infobox"><label style="font-weight:bold;text-transform: uppercase;">From Address:</label><hr><br/>' + results[0].formatted_address + '<br /><br /><button class="btn btn-block btn-primary btn-sm" onclick = "clear_dep();" value = "delete">Delete</button><br/></div>';
+      }
+      if (uniqueId == 2) {
+        label = 'To Address';
+        infoWindowcontent = '<div class="ib infobox"><label style="font-weight:bold;text-transform: uppercase;">To Address:</label><hr><br/>' + results[0].formatted_address + '<br /><br /><button class="btn btn-block btn-primary btn-sm" onclick = "clear_arr();" value = "delete">Delete</button><br/></div>';
+      }
+      if (marker == null) {
+        console.log(event.latLng);
+        marker = new google.maps.Marker({
+          position: event.latLng,
+          draggable: true,
+          label: {
+            text: label,
+            color: '#a2003b'
+          },
+
+          animation: google.maps.Animation.DROP,
+          map: map
+        });
+        marker.id = uniqueId;
+      } else {
+        marker.setPosition(event.latLng);
+      }
+      infowindow = new google.maps.InfoWindow({
+        content: infoWindowcontent
+      });
+      infowindow.open(map, marker);
+      uniqueId++;
+
+      //Add marker to the array.
+      markers.push(marker);
+    });
+  });
+}
+
+function createMarker(location, label, content, map, id) {
+  var marker = new google.maps.Marker({
+    position: location,
+    title: label,
+    id: id,
+    icon: {
+      url: 'https://maps.google.com/mapfiles/kml/pal4/icon31.png',
+      // This marker is 20 pixels wide by 32 pixels high.
+      // The anchor for this image is the base of the flagpole at (0, 32).
+      anchor: new google.maps.Point(0, 0)
+    },
+    map: map
+  });
+  infowindow = new google.maps.InfoWindow({
+    content: content,
+    maxWidth: 350
+  });
+  infowindow.setContent(content);
+  infowindow.open(map, marker);
+  markers.push(marker);
+}
+
+function set_lat_long(lat, lng, address, directionsService, directionsRenderer) {
+  var dep_lat = $('#dep_lat').val();
+  var dep_lng = $('#dep_lng').val();
+  var arr_lat = $('#arr_lat').val();
+  var arr_lng = $('#arr_lng').val();
+
+  if (isEmpty(dep_lat) || isEmpty(dep_lng)) {
+    //alert(dep_lat);
+    $('#dep_lat').val(lat);
+    $('#dep_lng').val(lng);
+    $('#departure_address').val(address);
+    $('#clear_dep').show();
+  } else {
+    if (isEmpty(arr_lat) || isEmpty(arr_lng)) {
+      $('#arr_lat').val(lat);
+      $('#arr_lng').val(lng);
+      $('#arrival_address').val(address);
+      $('#clear_arr,.arrival_address').show();
+    }
+  }
+
+  if (!isEmpty($('#dep_lat').val()) && !isEmpty($('#dep_lng').val()) && !isEmpty($('#arr_lat').val()) && !isEmpty($('#arr_lng').val())) calculateAndDisplayRoute(directionsService, directionsRenderer);
+}
+
+function isEmpty(value) {
+  return (value == null || value.length === 0);
+}
+initMap_mobile();
+
+    //// End maps with markets////
 
     // Register a widget that renders a map with a marker with the vehicle's location
     // Use the GoogleMapsFromSignal widget, then manually change the directions of the map created by accesing the DOM from box.window
