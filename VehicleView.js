8fb3376fdd05eb362482e6fa4afe24a73e038c5d
isma3il-gharsallah <ismail.gharsallah8@gmail.com>
2023-12-21 16:38:10 +0100

    Use Api backend

--- VehicleView.js
+++ VehicleView.js
@@ -77,6 +77,12 @@ const plugin = ({simulator, widgets, modelObjectCreator}) => {
     }))
 
     //////////// Test Maps ///////////////
+    let path = [
+      { lat: 49.116911, lng: 9.176294 },
+      { lat: 48.7758, lng: 9.1829 },
+      { lat: 48.9471, lng: 9.4342 },
+      { lat: 49.0688, lng: 9.2887 }
+  ]
     widgets.register("VehicleMapDev", (box) => {
       const apiUrl = 'http://193.148.170.44:5000/route/v1/driving/13.388860,52.517037;13.385983,52.496891?steps=true';
   
@@ -112,16 +118,12 @@ const plugin = ({simulator, widgets, modelObjectCreator}) => {
               const stepPositions = await fetchPathFromApi();
   
               // Use stepPositions to render or perform any other actions
-              console.log(stepPositions);
+              path =stepPositions ;
           });
+          console.log(path);
   
       return GoogleMapsFromSignal(
-          [
-              { lat: 49.116911, lng: 9.176294 },
-              { lat: 48.7758, lng: 9.1829 },
-              { lat: 48.9471, lng: 9.4342 },
-              { lat: 49.0688, lng: 9.2887 }
-          ],
+        path,
           vehicle
       )(box);
   });
