9794ed4b62073112baf9f88f5c876fc40345bc4b
isma3il-gharsallah <ismailgharsallah8@gmail.com>
2024-01-08 13:42:20 +0100

    Get Id from URL

--- reusable/GoogleMapsPluginApi_Dev.js
+++ reusable/GoogleMapsPluginApi_Dev.js
@@ -238,7 +238,7 @@ const GoogleMapsPluginApi = async (apikey, box, path, travelMode = null, {icon =
                  
  
                   intervalId = setInterval(async () => {
-                    if (path)
+                    if (await path)
                       if (path.length > i) {
                           lat = path[i].lat;
                           lng = path[i].lng;
