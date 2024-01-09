53fd58b3e4dc7d715fd643fafeae4e4595e0713f
isma3il-gharsallah <ismailgharsallah8@gmail.com>
2024-01-09 16:21:49 +0100

    Add Table VSS

--- VehicleView.js
+++ VehicleView.js
@@ -568,7 +568,141 @@ let intervalId3;
         box.injectNode(container);
  
     })
+  /////Table for test //////
+  widgets.register("TableVSS",  box => {
+    const container = document.createElement("div");
+    container.setAttribute("style", `display:block; ;overflow:auto;padding: 0px;`);
 
+    container.innerHTML = `
+    <div style="display: flex; height: 100%; width: 100%;">
+    <style>
+    @import url('https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,400;0,700;1,400;1,700&display=swap');
+    * {
+        box-sizing: border-box;
+    }
+    body {
+        font-family: 'Lato', sans-serif;
+    }
+    table {
+        display: grid;
+        height: fit-content;
+        min-height: 100%;
+        border-collapse: collapse;
+        min-width: 100%;
+        grid-template-columns: 
+            minmax(80px, 1fr)
+            minmax(80px, 1fr)
+        ;
+        grid-template-rows: min-content auto;
+        font-size: inherit;
+    }
+      
+    thead,
+    tbody,
+    tr {
+        display: contents;
+    }
+      
+    th,
+    td {
+        padding: 1em;
+        min-height: fit-content;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+        height: max-content;
+    }
+      
+    th {
+        position: sticky;
+        top: 0;
+        background: #6c7ae0;
+        user-select: none;
+        text-align: left;
+        font-weight: normal;
+        font-size: 1.1em;
+        color: white;
+        font-weight: bold;
+    }
+      
+    th:last-child {
+        border: 0;
+    }
+    
+    td:first-child {
+        font-weight: bold;
+    }
+
+    td {
+        padding-top: .66em;
+        padding-bottom: .66em;
+        background: transparent;
+        color: #808080;
+        height: 100%;
+    }
+      
+    tr:nth-child(even) td {
+        background: #f8f6ff;
+        color: #808080;
+    }
+
+    </style>
+        <div style="display: flex !important; height: 100%; width: 100%;">
+            <table>
+                <thead>
+                    <tr>
+                        <th>VSS API</th><th>Value</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    
+                        <tr data-api="Vehicle.VehicleIdentification.VIN">
+                            <td title="Vehicle.VehicleIdentification.VIN">Vehicle.VehicleIdentification.VIN</td><td title="">1GNCT18W9TEH9XV1E</td>
+                        </tr>
+                    
+                        <tr data-api="Vehicle.Speed">
+                            <td title="Vehicle.Speed">Vehicle.Speed</td><td title="">0</td>
+                        </tr>
+                    
+                        <tr data-api="Vehicle.Powertrain.TractionBattery.NetCapacity">
+                            <td title="Vehicle.Powertrain.TractionBattery.NetCapacity">Vehicle.Powertrain.TractionBattery.NetCapacity</td><td title="">80</td>
+                        </tr>
+                    
+                        <tr data-api="Vehicle.Powertrain.TractionBattery.Charging.ChargeLimit">
+                            <td title="Vehicle.Powertrain.TractionBattery.Charging.ChargeLimit">Vehicle.Powertrain.TractionBattery.Charging.ChargeLimit</td><td title="">94</td>
+                        </tr>
+                    
+                        <tr data-api="Vehicle.Powertrain.TractionBattery.StateOfCharge.Current">
+                            <td title="Vehicle.Powertrain.TractionBattery.StateOfCharge.Current">Vehicle.Powertrain.TractionBattery.StateOfCharge.Current</td><td title="">-0.17007927254561045</td>
+                        </tr>
+                    
+                        <tr data-api="Vehicle.Powertrain.TractionBattery.Range">
+                            <td title="Vehicle.Powertrain.TractionBattery.Range">Vehicle.Powertrain.TractionBattery.Range</td><td title="">-0.6803170901824418</td>
+                        </tr>
+                    
+                        <tr data-api="Vehicle.Powertrain.TractionBattery.Charging.IsCharging">
+                            <td title="Vehicle.Powertrain.TractionBattery.Charging.IsCharging">Vehicle.Powertrain.TractionBattery.Charging.IsCharging</td><td title="">false</td>
+                        </tr>
+                    
+                        <tr data-api="Vehicle.Powertrain.TractionBattery.Charging.TimeToComplete">
+                            <td title="Vehicle.Powertrain.TractionBattery.Charging.TimeToComplete">Vehicle.Powertrain.TractionBattery.Charging.TimeToComplete</td><td title="">1494.2690264734508</td>
+                        </tr>
+                    
+                </tbody>
+        </table>
+    </div>
+    </div>
+    `
+        
+      
+ 
+      
+
+      box.injectNode(container);
+
+  })
+
+    ///// End Table Test//////
  
     let Volkswagen=null;
     let Mercedes=null;
@@ -618,7 +752,7 @@ let intervalId3;
  
     })
 
-    // LineChart widget for StateOfCharge
+ 
 
     const StateOfChargeTile = {
         signal: "Vehicle.Powertrain.TractionBattery.StateOfCharge.Current",
