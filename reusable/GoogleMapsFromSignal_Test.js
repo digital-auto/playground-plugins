d97ac3ffb197415018fec353721c61df9b203a75
isma3il-gharsallah <ismailgharsallah8@gmail.com>
2024-01-11 11:14:11 +0100

    First commit

---
+++ reusable/GoogleMapsFromSignal_Test.js
@@ -0,0 +1,62 @@
+import GoogleMapsPluginApi from "./GoogleMapsPluginApi_Test.js"
+import { PLUGINS_APIKEY } from "./apikey.js"
+
+const supportsPins = (vehicle) => {
+    try {
+        vehicle.Next.get()
+        vehicle.Reset.get()
+        return true
+    } catch (error) {
+        return false
+    }
+}
+
+const convertCoordinates = (coordinates) => {
+    return {
+        lat: parseFloat(coordinates.lat),
+        lng: parseFloat(coordinates.lng)
+    }
+}
+
+const GoogleMapsFromSignal = (directions, vehicle, {
+
+    iterate = false,
+    autoNext = 800,
+    icon = null
+} = {}) => {
+    let getScore = async () => {
+        return await GoogleMapsPluginApi.getScore()
+    }
+    return (box) => {
+        console.log("GoogleMapsFromSignal icon", icon, autoNext)
+        let setVehiclePinGlobal = null
+        GoogleMapsPluginApi(PLUGINS_APIKEY, box, directions, null, {icon} ).then(({setVehiclePin}) => {
+            setVehiclePinGlobal = setVehiclePin
+        })
+
+        if (!supportsPins(vehicle) && iterate) {
+            alert("GoogleMapsFromSignal plugin doesn't support 'iterate' parameter without Wishlist sensors 'Vehicle.Next' and 'Vehicle.Reset'.")
+        }
+
+        const intervalId = setInterval(async () => {
+            if (setVehiclePinGlobal !== null) {
+                setVehiclePinGlobal(convertCoordinates({
+                    lat: await vehicle.CurrentLocation.Latitude.get(),
+                    lng: await vehicle.CurrentLocation.Longitude.get()
+                }))
+                if (iterate) {
+                    await vehicle.Next.get()
+                }
+            }
+        }, autoNext)
+
+
+        return () => {
+            if (intervalId !== null) {
+                clearInterval(intervalId)
+            }
+        }
+    }
+}
+
+export default GoogleMapsFromSignal
