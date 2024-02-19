aa28d6b32491f25497d15bc02671eb086fd8c43e
isma3il-gharsallah <ismailgharsallah8@gmail.com>
2024-02-19 12:18:06 +0100

    add legends

--- VehicleView.js
+++ VehicleView.js
@@ -538,7 +538,7 @@ let Charged=false;
 				<line class="mask" x1="50" y1="200" x2="50" y2="0" stroke="white" stroke-width="50" stroke-dasharray="200,200"/>
 				<line class="needle" x1="0" y1="0" x2="100" y2="0" stroke="rgb(156 163 175)" stroke-width="3" />
 			</svg>
-			<div id="message">Current battery SOC <img src="https://firebasestorage.googleapis.com/v0/b/digital-auto.appspot.com/o/media%2FEVIconWithOutCIF.png?alt=media&token=7337cfb3-b4f5-4b67-bfff-27a307396d4f" width="75" height="60"></div>
+			<div id="message">Current battery SOC <img src="https://firebasestorage.googleapis.com/v0/b/digital-auto.appspot.com/o/media%2FEVIconWithOutCIF.png?alt=media&token=7337cfb3-b4f5-4b67-bfff-27a307396d4f" width="40" height="30"></div>
 		</div>
     		<div id="scoreCar2" style="">
 			<div class="text">0.00%</div>
@@ -681,8 +681,8 @@ let Charged=false;
               color: #ECF0F1;
           }
           .message{
-            width: 200px;
-            font-size: 20px;
+            width: 250px;
+            font-size: 18px;
             margin-top:20px;
           }
           </style>
@@ -900,7 +900,7 @@ let Charged=false;
                 <thead>
                     <tr>
                         <th>VSS API</th><th>Value <img src="https://firebasestorage.googleapis.com/v0/b/digital-auto.appspot.com/o/media%2FEVIconWithOutCIF.png?alt=media&token=7337cfb3-b4f5-4b67-bfff-27a307396d4f" width="40" height="30">
-                        </th><th><img src="https://firebasestorage.googleapis.com/v0/b/digital-auto.appspot.com/o/media%2FEViconCIF.png?alt=media&token=4ef5caf0-f58a-4d84-924a-43db76cd6668" width="40" height="30"></th>
+                        </th><th>Value <img src="https://firebasestorage.googleapis.com/v0/b/digital-auto.appspot.com/o/media%2FEViconCIF.png?alt=media&token=4ef5caf0-f58a-4d84-924a-43db76cd6668" width="40" height="30"></th>
                     </tr>
                 </thead>
                 <tbody>
